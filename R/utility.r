783148f24f7bb59a5ee5fdf9e7d342d3c416cc30
jtlandis <jtlandis314@gmail.com>
2020-06-25 20:12:02 -0400

    Helper functions not exported by ggplot2

--- R/utility.r
+++ R/utility.r
@@ -1,12 +1,15 @@
+#'
 "%||%" <- function(a, b) {
   if (!is.null(a)) a else b
 }
 
+#'
 ggname <- function(prefix, grob) {
   grob$name <- grobName(grob, prefix)
   grob
 }
 
+#'
 empty <- function(df) {
   is.null(df) || nrow(df) == 0 || ncol(df) == 0 || is.waive(df)
 }
