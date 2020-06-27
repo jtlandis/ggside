817b57897cae419271ef39cbd32831226b7d7645
jtlandis <jtlandis314@gmail.com>
2020-06-27 18:56:22 -0400

    Creating a manual scale for later?

--- R/utility.r
+++ R/utility.r
@@ -18,5 +18,30 @@ empty <- function(df) {
   is.null(df) || nrow(df) == 0 || ncol(df) == 0 || is.waive(df)
 }
 
+manual_scale <- function(aesthetic, values = NULL, breaks = waiver(), ...) {
+  # check for missing `values` parameter, in lieu of providing
+  # a default to all the different scale_*_manual() functions
+  if (is_missing(values)) {
+    values <- NULL
+  } else {
+    force(values)
+  }
 
+  # order values according to breaks
+  if (is.vector(values) && is.null(names(values)) && !is.waive(breaks) &&
+      !is.null(breaks) && !is.function(breaks)) {
+    if (length(breaks) <= length(values)) {
+      names(values) <- breaks
+    } else {
+      names(values) <- breaks[1:length(values)]
+    }
+  }
 
+  pal <- function(n) {
+    if (n > length(values)) {
+      abort(glue("Insufficient values in manual scale. {n} needed but only {length(values)} provided."))
+    }
+    values
+  }
+  discrete_scale(aesthetic, "manual", pal, breaks = breaks, ...)
+}
