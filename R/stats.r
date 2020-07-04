b894fba9807884c94bc4fe3f2632af2e28c7b359
jtlandis <jtlandis314@gmail.com>
2020-07-04 13:15:32 -0400

    so far so good with stat_summarise

--- R/stats.r
+++ R/stats.r
@@ -131,22 +131,20 @@
 #                          })
 #                        })
 
-
+#' @rdname stat_summarise
+#' @name StatSummarise
+#' @aliases StatSummarisex StatSummarisey StatSummarizex StatSummarizey
 StatSummarise <- ggplot2::ggproto("Summarise",
                                 Stat,
-                                required_aes = c("x|y", "xfill|yfill"),
-                                option_aes = c("yintercept|xintercept"),
-                                compute_panel = function(self, data, scales, location = NULL,
-                                                         height = NULL,width = NULL,
-                                                         yintercept = NULL, xintercept = NULL,
+                                required_aes = c("x|y",  "domain|domain"),
+                                by = NULL,
+                                compute_panel = function(self, data, scales,
+                                                         by = NULL, domain = NULL,
                                                          fun = NULL, fun.args = list()) {
                                   #
                                   #browser()
-                                  x_ <- data$xintercept %||% xintercept %||% NULL
-                                  y_ <- data$yintercept %||% yintercept %||% NULL
-                                  env <- find_build_plotEnv()
-                                  .hs <- get_variable(".build_history", envir = env) %||% data_frame(loc = c("top","right","bottom","left"),
-                                                                                                     indx = c(1,1,1,1))
+                                  by <- by %||% self$by
+
                                   if(is.null(fun)) {
                                     warn("fun is null, using mean as default")
                                     fun <- mean
@@ -158,85 +156,12 @@ StatSummarise <- ggplot2::ggproto("Summarise",
                                   }
 
                                   if (empty(data)) return(new_data_frame())
-                                  #determine if xfill or yfill was passed...
-                                  is_xbar <- "xfill"%in%colnames(data)
-                                  if(is_xbar){
-                                    fun.data <- data %>%
-                                      group_by(x) %>%
-                                      dplyr::summarise(xfill=call_f(fun, xfill))
-                                    data <- select(data,-xfill) %>%
-                                      left_join(., y = fun.data, by = "x")
-                                    #check if y exists in data, if not assign 0
-                                    data$y <- data$y %||% 0
-                                    #check resolution of y data to see how far to adjust
-                                    yres <- if(resolution(data$y, FALSE)!=1) (diff(range(data$y))*.1) else 1
-                                    #provide the height adjustment. If none specified, use yres
-                                    data$height <- data$height %||% height %||% yres
-                                    if(any(data$height<0)){
-                                      warn("height has been given a negative value. Using abosulte value")
-                                      data$height <- abs(data$height)
-                                    }
-                                    data$width <- data$width %||% width %||% resolution(data$x, FALSE)
-                                    data$location <- data$location %||% location %||% "bottom"
-                                    data <- mutate(data, group = x)
-                                    if(is.null(y_)){ #e.i. user prefer's StatSidebar to determine y positions
-                                      if(!all(data$location%in%c("top","bottom"))){
-                                        .badLoc <- !data$location%in%c("top","bottom")
-                                        warn(glue("unrecognized location assignments:\n",
-                                                  "{paste(unique(data$location[.badLoc]),collapse = \", \")}",
-                                                  "\nConverting to default: bottom"))
-                                        data$location[.badLoc] <- "bottom"
-                                      }
-                                      data <- left_join(data, .hs, by = c("location"="loc")) %>%
-                                        mutate(y = case_when(location=="bottom" ~ min(y)-yres*indx + ((yres-height)/2),
-                                                             location=="top" ~ max(y)+yres*indx - ((yres-height)/2)))
 
-                                    } else {
-                                      data$y <- y_
-                                    }
-                                  } else {
-                                    fun.data <- data %>%
-                                      group_by(y) %>%
-                                      dplyr::summarise(yfill=call_f(fun, yfill))
-                                    data <- select(data,-yfill) %>%
-                                      left_join(., y = fun.data, by = "y")
-                                    #check if x exists in data, if not assign 0
-                                    data$x <- data$x %||% 0
-                                    #check resolution of x data to see how far to adjust
-                                    xres <- if(resolution(data$x, FALSE)!=1) (diff(range(data$x))*.1) else 1
-                                    #provide the width adjustment. If none specified, use xres
-                                    data$width <- data$width %||% width %||% xres
-                                    if(any(data$width<0)){
-                                      warn("width has been given a negative value. Using abosulte value")
-                                      data$width <- abs(data$width)
-                                    }
-                                    data$height <- data$height %||% height %||% resolution(data$y, FALSE)
-                                    data$location <- data$location %||% location %||% "left"
-                                    data <- mutate(data, group = y)
-                                    if(is.null(x_)){ #e.i. user prefer's StatSidebar to determine x positions
-                                      if(!all(data$location%in%c("right","left"))){
-                                        .badLoc <- !data$location%in%c("right","left")
-                                        warn(glue("unrecognized location assignments:\n",
-                                                  "{paste(unique(data$location[.badLoc]),collapse = \", \")}",
-                                                  "\nConverting to default: left"))
-                                        data$location[.badLoc] <- "left"
-                                      }
-                                      data <-  left_join(data, .hs, by = c("location"="loc")) %>%
-                                        mutate(x = case_when(location=="left" ~ min(x)-xres*indx + ((xres-width)/2),
-                                                             location=="right" ~ max(x)+xres*indx - ((xres-width)/2)))
+                                  fun.data <- data %>%
+                                    group_by_(.dots = by) %>%
+                                    dplyr::summarise(summarise = call_f(fun, domain))
+                                  data <- left_join(data, fun.data, by = by)
 
-                                    } else {
-                                      data$x <- x_
-                                    }
-                                  }
-
-                                  data <- data[,!colnames(data)%in%"indx"] %>% distinct_all()
-                                  suppress.Location <- is.null(x_)&is.null(y_)
-                                  if(suppress.Location){
-                                    logi <- .hs$loc %in% data$location
-                                    .hs[logi,"indx"] <- .hs[logi,"indx"] + 1
-                                    assign(".build_history", value = .hs, envir = env)
-                                  }
                                   data
                                 },
                                 compute_group = function(self, data, scales){
@@ -274,3 +199,22 @@ StatSummarise <- ggplot2::ggproto("Summarise",
                                     })
                                   })
                                 })
+#' @rdname stat_summarise
+StatSummarisey <- ggplot2::ggproto("Summarisey",
+                                   StatSummarise,
+                                   by = "y")
+#' @rdname stat_summarise
+StatSummarisex <- ggplot2::ggproto("Summarisex",
+                                   StatSummarise,
+                                   by = "x")
+#' @rdname stat_summarise
+StatSummarize <- ggplot2::ggproto("Summarize",
+                                  StatSummarise)
+#' @rdname stat_summarise
+StatSummarizey <- ggplot2::ggproto("Summarizey",
+                                   StatSummarize,
+                                   by = "y")
+#' @rdname stat_summarise
+StatSummarizex <- ggplot2::ggproto("Summarizex",
+                                   StatSummarize,
+                                   by = "x")
