9324aa6a67eca86fe9a7dd91bcf0d45a9351dbf4
jtlandis <jtlandis314@gmail.com>
2020-07-16 17:54:36 -0400

    need to be sure it takes the correct params

--- R/geom-sidedensity.r
+++ R/geom-sidedensity.r
@@ -38,8 +38,8 @@ GeomXsidedensity <- ggplot2::ggproto("GeomXsidedensity",
                                      draw_group = function(data, panel_params, coord, na.rm = FALSE, flipped_aes = FALSE, outline.type = "both") {
                                        #browser()
                                        data <- use_xside_aes(data)
-                                       ggplot2::GeomDensity$draw_group(data, panel_params, coord,na.rm = FALSE,
-                                                                       flipped_aes = FALSE, outline.type = "both")},
+                                       ggplot2::GeomDensity$draw_group(data = data, panel_params = panel_params, coord = coord, na.rm = na.rm,
+                                                                       flipped_aes = flipped_aes, outline.type = outline.type)},
                                      draw_key = function(data, params, size) {
                                        data <- use_xside_aes(data)
                                        ggplot2::GeomDensity$draw_key(data, params, size)
@@ -77,17 +77,16 @@ geom_ysidedensity <- function(mapping = NULL, data = NULL,
 GeomYsidedensity <- ggplot2::ggproto("GeomYsidedensity",
                                      ggplot2::GeomDensity,
                                      default_aes = aes(fill = NA, yfill = NA, weight = 1,
-                                                       colour = "black", xcolour = NA, alpha = NA,
+                                                       colour = "black", ycolour = NA, alpha = NA,
                                                        size = 0.5, linetype = 1),
                                      setup_data = function(data, params) {
                                        data <- parse_side_aes(data, params)
                                        ggplot2::GeomDensity$setup_data(data, params)
                                      },
                                      draw_group = function(data, panel_params, coord, na.rm = FALSE, flipped_aes = FALSE, outline.type = "both") {
-                                       #browser()
                                        data <- use_yside_aes(data)
-                                       ggplot2::GeomDensity$draw_group(data, panel_params, coord, na.rm = FALSE,
-                                                                       flipped_aes = FALSE, outline.type = "both")
+                                       ggplot2::GeomDensity$draw_group(data = data, panel_params = panel_params, coord = coord, na.rm = na.rm,
+                                                                       flipped_aes = flipped_aes, outline.type = outline.type)
                                      },
                                      draw_key = function(data, params, size) {
                                        data <- use_yside_aes(data)
