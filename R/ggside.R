4c6f07b71f6fc00b4427a3e8f104bfb2c2231811
jtlandis <jtlandis314@gmail.com>
2020-07-15 21:04:58 -0400

    ggside

---
+++ R/ggside.R
@@ -0,0 +1,17 @@
+#' @rdname ggside
+#' @description Set characteristics of side panels
+#' @param x.pos x side panel can either take "top" or "bottom"
+#' @param y.pos y side panel can either take "right" or "left"
+#' @param scales Determines side panel's unaligned axis
+#' scale. Inputs are similar to facet_* scales function. Default
+#' is set to "fixed", but "free_x", "free_y" and "free" are
+#' acceptable inputs. For example, xside panels are aligned to
+#' the x axis of the main panel. Setting "free" or "free_y" will
+#' cause all y scales of the x side Panels to be independent.
+#'
+#' @export
+ggside <- function(x.pos = "top", y.pos = "right", scales = "fixed"){
+  structure(list(x.pos = x.pos,
+                 y.pos = y.pos,
+                 scales = scales), class = "ggside")
+}
