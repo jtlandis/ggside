021348051d01bf641f3ba6aa615b653c96b579c1
jtlandis <jtlandis314@gmail.com>
2021-01-21 21:48:23 -0500

    need to export +.gg

--- R/add_gg.R
+++ R/add_gg.R
@@ -1,14 +1,16 @@
 
-
+#' @export
 `+.gg` <- function(e1, e2) {
   UseMethod("+.gg")
 }
 
+#' @export
 `+.gg.default` <- ggplot2:::`+.gg`
 
+#' @export
 `+.gg.ggside` <- function(e1, e2) {
   browser()
-  plot <- NextMethod()
+  plot <- NextMethod("+.gg")
   if(!is.ggside_layer(e2)){
     plot <- make_ggside(plot, e2)
   }
