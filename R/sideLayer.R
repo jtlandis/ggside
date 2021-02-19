9007f1c2964e71f6cdab4868673d46862c949ad1
jtlandis <jtlandis314@gmail.com>
2021-02-18 22:51:45 -0500

    Stealing from ggplot2 to avoid ggplot2:::Layer usage - Causes a note in R CMD

--- R/sideLayer.R
+++ R/sideLayer.R
@@ -3,14 +3,14 @@
 
 
 XLayer <- ggplot2::ggproto("XLayer",
-                           ggplot2:::Layer,
+                           Layer,
                            setup_layer = function(self, data, plot){
                              data$PANEL_TYPE <- "x"
                              data
                            })
 
 YLayer <- ggplot2::ggproto("YLayer",
-                           ggplot2:::Layer,
+                           Layer,
                            setup_layer = function(self, data, plot){
                              data$PANEL_TYPE <- "y"
                              data
