bee65d48f6f582bb8e5d4fe720dc13fdb92d1eeb
jtlandis <jtlandis314@gmail.com>
2021-08-16 15:29:29 -0400

    explicitly exporting ggplot_add

--- R/plot-construction.R
+++ R/plot-construction.R
@@ -53,15 +53,20 @@ update_ggside.ggplot <- function(object, ggside = NULL){
   return(object)
 }
 
+#' @importFrom ggplot2 ggplot_add
+#' @export
 ggplot_add.ggside_layer <- function(object, plot, object_name){
   p <- NextMethod("ggplot_add")
   as_ggside(p)
 }
 
+#' @export
 ggplot_add.ggside_options <- function(object, plot, object_name){
   as_ggside(plot, object)
 }
 
+
+#' @export
 ggplot_add.ggside_scale <- function(object, plot, object_name){
   plot$ggside[[intersect(c("xsidey","ysidex"), object$aesthetics)]] <- object #save scale in appropriate place
   as_ggside(plot)
