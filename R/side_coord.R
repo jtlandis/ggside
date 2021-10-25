262ba9012d53d9d43b7551bd785ec79ed8fb350e
Justin Landis <jtlandis@pop-os.localdomain>
2021-10-25 15:39:13 -0400

    adding control for rendering vertical and horizontal axis in Coord object

--- R/side_coord.R
+++ R/side_coord.R
@@ -51,8 +51,292 @@ CoordSideCartesian <- ggplot2::ggproto(
     } else {
       element_render(theme, "panel.border")
     }
+  },
+  render_axis_h = function (panel_params, theme) {
+    panel_type <- panel_params$ggside_panel_type
+
+    if (is.element(panel_type, c("x", "main"))) {
+      list(top = panel_guides_grob(panel_params$guides, position = "top", theme = theme),
+           bottom = panel_guides_grob(panel_params$guides, position = "bottom", theme = theme))
+    } else {
+      list(top = ggside_panel_guides_grob(panel_params$guides, position = "top", theme = theme),
+           bottom = ggside_panel_guides_grob(panel_params$guides, position = "bottom", theme = theme))
+    }
+  },
+  render_axis_v = function (panel_params, theme) {
+    panel_type <- panel_params$ggside_panel_type
+
+    if (is.element(panel_type, c("y", "main"))) {
+      list(left = panel_guides_grob(panel_params$guides, position = "left", theme = theme),
+           right = panel_guides_grob(panel_params$guides, position = "right", theme = theme))
+    } else {
+      list(left = ggside_panel_guides_grob(panel_params$guides, position = "left", theme = theme),
+           right = ggside_panel_guides_grob(panel_params$guides, position = "right", theme = theme))
+    }
   }
 )
+
+panel_guides_grob <- function (guides, position, theme)
+{
+  guide <- guide_for_position(guides, position) %||% guide_none()
+  guide_gengrob(guide, theme)
+}
+
+as_ggside_axis <- function(x) {
+  if (inherits(x, "guide_none")) return(x)
+  class(x) <- c("ggside_axis", class(x))
+  x
+}
+
+guide_for_position <- function (guides, position)
+{
+  has_position <- vapply(guides, function(guide) identical(guide$position,
+                                                           position), logical(1))
+  guides <- guides[has_position]
+  guides_order <- vapply(guides, function(guide) as.numeric(guide$order)[1],
+                         numeric(1))
+  Reduce(guide_merge, guides[order(guides_order)])
+}
+
+ggside_panel_guides_grob <- function(guides, position, theme) {
+  guide <- guide_for_position(guides, position) %||% guide_none()
+  guide <- as_ggside_axis(guide)
+  guide_gengrob(guide, theme)
+}
+
+#' @importFrom ggplot2 guide_gengrob
+#' @export
+guide_gengrob.ggside_axis <- function(guide, theme) {
+  aesthetic <- names(guide$key)[!grepl("^\\.", names(guide$key))][1]
+  draw_ggside_axis(break_positions = guide$key[[aesthetic]], break_labels = guide$key$.label,
+                   axis_position = guide$position, theme = theme, check.overlap = guide$check.overlap,
+                   angle = guide$angle, n.dodge = guide$n.dodge)
+}
+
+axis_label_element_overrides <- function (axis_position, angle = NULL) {
+  if (is.null(angle)) {
+    return(element_text(angle = NULL, hjust = NULL, vjust = NULL))
+  }
+  if (angle > 90 || angle < -90) {
+    abort("`angle` must be between 90 and -90")
+  }
+  if (axis_position == "bottom") {
+    element_text(angle = angle, hjust = if (angle > 0)
+      1
+      else if (angle < 0)
+        0
+      else 0.5, vjust = if (abs(angle) == 90)
+        0.5
+      else 1)
+  }
+  else if (axis_position == "left") {
+    element_text(angle = angle, hjust = if (abs(angle) ==
+                                            90)
+      0.5
+      else 1, vjust = if (angle > 0)
+        0
+      else if (angle < 0)
+        1
+      else 0.5, )
+  }
+  else if (axis_position == "top") {
+    element_text(angle = angle, hjust = if (angle > 0)
+      0
+      else if (angle < 0)
+        1
+      else 0.5, vjust = if (abs(angle) == 90)
+        0.5
+      else 0)
+  }
+  else if (axis_position == "right") {
+    element_text(angle = angle, hjust = if (abs(angle) ==
+                                            90)
+      0.5
+      else 0, vjust = if (angle > 0)
+        1
+      else if (angle < 0)
+        0
+      else 0.5, )
+  }
+  else {
+    abort(glue("Unrecognized position: '{axis_position}'"))
+  }
+}
+# setup_ggside_axis_theme <- function(theme) {
+#   # text
+#   theme[["ggside.axis.text"]] <- theme[["ggside.axis.text"]] %||% theme[["axis.text"]]
+#   theme[["ggside.axis.text.x"]] <- theme[["ggside.axis.text.x"]] %||% combine_elements(theme[["ggside.axis.text"]], theme[["axis.text.x"]])
+#   theme[["ggside.axis.text.x.top"]] <- theme[["ggside.axis.text.x.top"]] %||% theme[["ggside.axis.text.x"]]
+#   theme[["ggside.axis.text.x.bottom"]] <- theme[["ggside.axis.text.x.bottom"]] %||% theme[["ggside.axis.text.x"]]
+#   theme[["ggside.axis.text.y"]] <- theme[["ggside.axis.text.y"]] %||% combine_elements(theme[["ggside.axis.text"]], theme[["axis.text.y"]])
+#   theme[["ggside.axis.text.y.left"]] <- theme[["ggside.axis.text.y.left"]] %||% theme[["ggside.axis.text.y"]]
+#   theme[["ggside.axis.text.y.right"]] <- theme[["ggside.axis.text.y.right"]] %||% theme[["ggside.axis.text.y"]]
+#   # line
+#   theme[["ggside.axis.line"]] <- theme[["ggside.axis.line"]] %||% theme[["axis.line"]]
+#   theme[["ggside.axis.line.x"]] <- theme[["ggside.axis.line.x"]] %||% combine_elements(theme[["ggside.axis.line"]], theme[["axis.line.x"]])
+#   theme[["ggside.axis.line.x.top"]] <- theme[["ggside.axis.line.x.top"]] %||% theme[["ggside.axis.line.x"]]
+#   theme[["ggside.axis.line.x.bottom"]] <- theme[["ggside.axis.line.x.bottom"]] %||% theme[["ggside.axis.line.x"]]
+#   theme[["ggside.axis.line.y"]] <- theme[["ggside.axis.line.y"]] %||% combine_elements(theme[["ggside.axis.line"]], theme[["axis.line.y"]])
+#   theme[["ggside.axis.line.y.left"]] <- theme[["ggside.axis.line.y.left"]] %||% theme[["ggside.axis.line.y"]]
+#   theme[["ggside.axis.line.y.right"]] <- theme[["ggside.axis.line.y.right"]] %||% theme[["ggside.axis.line.y"]]
+#   # ticks
+#   theme[["ggside.axis.ticks"]] <- theme[["ggside.axis.ticks"]] %||% theme[["axis.ticks"]]
+#   theme[["ggside.axis.ticks.x"]] <- theme[["ggside.axis.ticks.x"]] %||% combine_elements(theme[["ggside.axis.ticks"]], theme[["axis.ticks.x"]])
+#   theme[["ggside.axis.ticks.x.top"]] <- theme[["ggside.axis.ticks.x.top"]] %||% theme[["ggside.axis.ticks.x"]]
+#   theme[["ggside.axis.ticks.x.bottom"]] <- theme[["ggside.axis.ticks.x.bottom"]] %||% theme[["ggside.axis.ticks.x"]]
+#   theme[["ggside.axis.ticks.y"]] <- theme[["ggside.axis.ticks.y"]] %||% combine_elements(theme[["ggside.axis.ticks"]], theme[["axis.ticks.y"]])
+#   theme[["ggside.axis.ticks.y.left"]] <- theme[["ggside.axis.ticks.y.left"]] %||% theme[["ggside.axis.ticks.y"]]
+#   theme[["ggside.axis.ticks.y.right"]] <- theme[["ggside.axis.ticks.y.right"]] %||% theme[["ggside.axis.ticks.y"]]
+#   # ticks.length
+#   theme[["ggside.axis.ticks.length"]] <- theme[["ggside.axis.ticks.length"]] %||% theme[["axis.ticks.length"]]
+#   theme[["ggside.axis.ticks.length.x"]] <- theme[["ggside.axis.ticks.length.x"]] %||% combine_elements(theme[["ggside.axis.ticks.length"]], theme[["axis.ticks.length.x"]])
+#   theme[["ggside.axis.ticks.length.x.top"]] <- theme[["ggside.axis.ticks.length.x.top"]] %||% theme[["ggside.axis.ticks.length.x"]]
+#   theme[["ggside.axis.ticks.length.x.bottom"]] <- theme[["ggside.axis.ticks.length.x.bottom"]] %||% theme[["ggside.axis.ticks.length.x"]]
+#   theme[["ggside.axis.ticks.length.y"]] <- theme[["ggside.axis.ticks.length.y"]] %||% combine_elements(theme[["ggside.axis.ticks.length"]], theme[["axis.ticks.length.y"]])
+#   theme[["ggside.axis.ticks.length.y.left"]] <- theme[["ggside.axis.ticks.length.y.left"]] %||% theme[["ggside.axis.ticks.length.y"]]
+#   theme[["ggside.axis.ticks.length.y.right"]] <- theme[["ggside.axis.ticks.length.y.right"]] %||% theme[["ggside.axis.ticks.length.y"]]
+#   theme
+# }
+
+draw_ggside_axis <- function (break_positions, break_labels, axis_position, theme,
+          check.overlap = FALSE, angle = NULL, n.dodge = 1)
+{
+
+  axis_position <- match.arg(axis_position, c("top", "bottom",
+                                              "right", "left"))
+  aesthetic <- if (axis_position %in% c("top", "bottom"))  "x" else "y"
+
+  line_element_name <- paste0("ggside.axis.line.", aesthetic, ".",
+                              axis_position)
+  tick_element_name <- paste0("ggside.axis.ticks.", aesthetic, ".",
+                              axis_position)
+  tick_length_element_name <- paste0("ggside.axis.ticks.length.",
+                                     aesthetic, ".", axis_position)
+  label_element_name <- paste0("ggside.axis.text.", aesthetic, ".",
+                               axis_position)
+  line_element <- calc_element(line_element_name, theme)
+  tick_element <- calc_element(tick_element_name, theme)
+  tick_length <- calc_element(tick_length_element_name, theme)
+  label_element <- calc_element(label_element_name, theme)
+  if (inherits(label_element, "element_text")) {
+    label_overrides <- axis_label_element_overrides(axis_position,
+                                                    angle)
+    if (!is.null(label_overrides$angle)) {
+      label_element$angle <- label_overrides$angle
+    }
+    if (!is.null(label_overrides$hjust)) {
+      label_element$hjust <- label_overrides$hjust
+    }
+    if (!is.null(label_overrides$vjust)) {
+      label_element$vjust <- label_overrides$vjust
+    }
+  }
+  is_vertical <- axis_position %in% c("left", "right")
+  position_dim <- if (is_vertical)
+    "y"
+  else "x"
+  non_position_dim <- if (is_vertical)
+    "x"
+  else "y"
+  position_size <- if (is_vertical)
+    "height"
+  else "width"
+  non_position_size <- if (is_vertical)
+    "width"
+  else "height"
+  gtable_element <- if (is_vertical)
+    gtable_row
+  else gtable_col
+  measure_gtable <- if (is_vertical)
+    gtable_width
+  else gtable_height
+  measure_labels_non_pos <- if (is_vertical)
+    grobWidth
+  else grobHeight
+  is_second <- axis_position %in% c("right", "top")
+  tick_direction <- if (is_second)
+    1
+  else -1
+  non_position_panel <- if (is_second)
+    unit(0, "npc")
+  else unit(1, "npc")
+  tick_coordinate_order <- if (is_second)
+    c(2, 1)
+  else c(1, 2)
+  labels_first_gtable <- axis_position %in% c("left", "top")
+  n_breaks <- length(break_positions)
+  opposite_positions <- c(top = "bottom", bottom = "top", right = "left",
+                          left = "right")
+  axis_position_opposite <- unname(opposite_positions[axis_position])
+  line_grob <- exec(element_grob, line_element, `:=`(!!position_dim,
+                                                     unit(c(0, 1), "npc")), `:=`(!!non_position_dim, unit.c(non_position_panel,
+                                                                                                            non_position_panel)))
+  if (n_breaks == 0) {
+    return(absoluteGrob(gList(line_grob), width = grobWidth(line_grob),
+                        height = grobHeight(line_grob)))
+  }
+  if (is.list(break_labels)) {
+    if (any(vapply(break_labels, is.language, logical(1)))) {
+      break_labels <- do.call(expression, break_labels)
+    }
+    else {
+      break_labels <- unlist(break_labels)
+    }
+  }
+  dodge_pos <- rep(seq_len(n.dodge), length.out = n_breaks)
+  dodge_indices <- split(seq_len(n_breaks), dodge_pos)
+  label_grobs <- lapply(dodge_indices, function(indices) {
+    draw_axis_labels(break_positions = break_positions[indices],
+                     break_labels = break_labels[indices], label_element = label_element,
+                     is_vertical = is_vertical, check.overlap = check.overlap)
+  })
+  ticks_grob <- exec(element_grob, tick_element, `:=`(!!position_dim,
+                                                      rep(unit(break_positions, "native"), each = 2)), `:=`(!!non_position_dim,
+                                                                                                            rep(unit.c(non_position_panel + (tick_direction * tick_length),
+                                                                                                                       non_position_panel)[tick_coordinate_order], times = n_breaks)),
+                     id.lengths = rep(2, times = n_breaks))
+  non_position_sizes <- paste0(non_position_size, "s")
+  label_dims <- do.call(unit.c, lapply(label_grobs, measure_labels_non_pos))
+  grobs <- c(list(ticks_grob), label_grobs)
+  grob_dims <- unit.c(max(tick_length, unit(0, "pt")), label_dims)
+  if (labels_first_gtable) {
+    grobs <- rev(grobs)
+    grob_dims <- rev(grob_dims)
+  }
+  gt <- exec(gtable_element, name = "axis", grobs = grobs,
+             `:=`(!!non_position_sizes, grob_dims), `:=`(!!position_size,
+                                                         unit(1, "npc")))
+  justvp <- exec(viewport, `:=`(!!non_position_dim, non_position_panel),
+                 `:=`(!!non_position_size, measure_gtable(gt)), just = axis_position_opposite)
+  absoluteGrob(gList(line_grob, gt), width = gtable_width(gt),
+               height = gtable_height(gt), vp = justvp)
+}
+
+absoluteGrob <- function (grob, width = NULL, height = NULL, xmin = NULL, ymin = NULL,
+          vp = NULL) {
+  gTree(children = grob, width = width, height = height, xmin = xmin,
+        ymin = ymin, vp = vp, cl = "absoluteGrob")
+}
+
+draw_axis_labels <- function (break_positions, break_labels, label_element, is_vertical,
+                              check.overlap = FALSE) {
+  position_dim <- if (is_vertical)
+    "y"
+  else "x"
+  label_margin_name <- if (is_vertical)
+    "margin_x"
+  else "margin_y"
+  n_breaks <- length(break_positions)
+  break_positions <- unit(break_positions, "native")
+  if (check.overlap) {
+    priority <- axis_label_priority(n_breaks)
+    break_labels <- break_labels[priority]
+    break_positions <- break_positions[priority]
+  }
+  labels_grob <- exec(element_grob, label_element, `:=`(!!position_dim,
+                                                        break_positions), `:=`(!!label_margin_name, TRUE), label = break_labels,
+                      check.overlap = check.overlap)
+}
 
 combine_elements <- function (e1, e2) {
   if (is.null(e2) || inherits(e1, "element_blank")) {
@@ -102,20 +386,19 @@ guide_grid <- function (theme, x.minor, x.major, y.minor, y.major) {
                                    id.lengths = rep(2, length(x.major)))))
 }
 
-setup_ggside_panel_theme <- function(theme) {
-  theme[["ggside.panel.background"]] <- theme[["ggside.panel.background"]] %||% theme[["panel.background"]]
-  theme[["ggside.panel.grid"]] <- theme[["ggside.panel.grid"]] %||% theme[["panel.grid"]]
-  theme[["ggside.panel.grid.major"]] <- theme[["ggside.panel.grid.major"]] %||% theme[["panel.grid.major"]] %||% theme[["ggside.panel.grid"]]
-  theme[["ggside.panel.grid.major.x"]] <- theme[["ggside.panel.grid.major.x"]] %||% theme[["panel.grid.major.x"]] %||% theme[["ggside.panel.grid"]]
-  theme[["ggside.panel.grid.major.y"]] <- theme[["ggside.panel.grid.major.y"]] %||% theme[["panel.grid.major.y"]] %||% theme[["ggside.panel.grid"]]
-  theme[["ggside.panel.grid.minor"]] <- theme[["ggside.panel.grid.minor"]] %||% combine_elements(theme[["ggside.panel.grid"]], theme[["panel.grid.minor"]])
-  theme[["ggside.panel.grid.minor.x"]] <- theme[["ggside.panel.grid.minor.x"]] %||% theme[["panel.grid.minor.x"]] %||% theme[["ggside.panel.grid.minor"]]
-  theme[["ggside.panel.grid.minor.y"]] <- theme[["ggside.panel.grid.minor.y"]] %||% theme[["panel.grid.minor.y"]] %||% theme[["ggside.panel.grid.minor"]]
-  theme
-}
+# setup_ggside_panel_theme <- function(theme) {
+#   theme[["ggside.panel.background"]] <- theme[["ggside.panel.background"]] %||% theme[["panel.background"]]
+#   theme[["ggside.panel.grid"]] <- theme[["ggside.panel.grid"]] %||% theme[["panel.grid"]]
+#   theme[["ggside.panel.grid.major"]] <- theme[["ggside.panel.grid.major"]] %||% theme[["panel.grid.major"]] %||% theme[["ggside.panel.grid"]]
+#   theme[["ggside.panel.grid.major.x"]] <- theme[["ggside.panel.grid.major.x"]] %||% theme[["panel.grid.major.x"]] %||% theme[["ggside.panel.grid"]]
+#   theme[["ggside.panel.grid.major.y"]] <- theme[["ggside.panel.grid.major.y"]] %||% theme[["panel.grid.major.y"]] %||% theme[["ggside.panel.grid"]]
+#   theme[["ggside.panel.grid.minor"]] <- theme[["ggside.panel.grid.minor"]] %||% combine_elements(theme[["ggside.panel.grid"]], theme[["panel.grid.minor"]])
+#   theme[["ggside.panel.grid.minor.x"]] <- theme[["ggside.panel.grid.minor.x"]] %||% theme[["panel.grid.minor.x"]] %||% theme[["ggside.panel.grid.minor"]]
+#   theme[["ggside.panel.grid.minor.y"]] <- theme[["ggside.panel.grid.minor.y"]] %||% theme[["panel.grid.minor.y"]] %||% theme[["ggside.panel.grid.minor"]]
+#   theme
+# }
 
 ggside_guide_grid <- function(theme, x.minor, x.major, y.minor, y.major) {
-    theme <- setup_ggside_panel_theme(theme)
     x.minor <- setdiff(x.minor, x.major)
     y.minor <- setdiff(y.minor, y.major)
     ggname("grill",
