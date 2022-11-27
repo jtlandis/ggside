ff08cf72267f3e9d31b847e6cd6aa7cb41bce091
jtlandis <jtlandis314@gmail.com>
2022-11-27 15:41:34 -0500

    making `ggplot_build.ggside`

--- R/ggplot_build.r
+++ R/ggplot_build.r
@@ -2,9 +2,11 @@
 #' @export
 ggplot_build.ggside <- function(plot) {
 
-  plot$scales <- make_new_scale_list(plot$scales, plot$ggside)
+  plot$scales <- ggside_scales(plot$scales)
   plot$facet <- as_ggsideFacet(plot$facet, plot$ggside)
   plot$coordinates <- as_ggsideCoord(plot$coordinates)
-  NextMethod("ggplot_build")
+
+  built <- NextMethod("ggplot_build")
+  standardise_panel_params(built)
 
 }
