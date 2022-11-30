99ea0ca7ef5611db9ab9651f3df98046351f9e16
jtlandis <jtlandis314@gmail.com>
2022-11-30 11:08:14 -0500

    Continuing to modify implementation of XLayer and YLayer

--- R/side-layer.R
+++ R/side-layer.R
@@ -29,18 +29,36 @@ new_ggside_layer <- function(side = c("x","y"), geom = NULL, stat = NULL, data =
     "ggside_layer",
     parent_layer,
     setup_layer = function(self, data, plot) {
+      names(plot$mapping) <- rename_side(names(plot$mapping), self$.side)
+      plot$mapping <- drop_plot_aes(plot$mapping, self$mapping, self$.side)
       data <- ggproto_parent(parent_layer, self)$setup_layer(data, plot)
       data[["PANEL_TYPE"]] <- self$.side
       data
     },
     compute_aesthetics = function(self, data, plot) {
-      data <- ggproto_parent(parent_layer, self)$compute_aesthetics(data, plot)
-      data[, setdiff(names(data), self$.other), drop = FALSE]
+      parent <- ggproto_parent(parent_layer, self)
+      data <- parent$compute_aesthetics(data, plot)
+      aes_to_drop <- self$.other
+      if (all(paste0(c(self$.side, ""), "colour") %in% names(data))) {
+        aes_to_drop <- c(aes_to_drop, "colour")
+      }
+      if (all(paste0(c(self$.side, ""), "fill") %in% names(data))) {
+        aes_to_drop <- c(aes_to_drop, "fill")
+      }
+      data[, setdiff(names(data), aes_to_drop), drop = FALSE]
     },
     compute_statistic = function(self, data, layout) {
       data <- self$geom$.data_unmapper(data)
       data <- ggproto_parent(parent_layer, self)$compute_statistic(data, layout)
       self$geom$.data_mapper(data)
+    },
+    map_statistic = function(self, data, plot) {
+      old_nms <- names(self$stat$default_aes)
+      names(self$stat$default_aes) <- rename_side(names(self$stat$default_aes), self$.side)
+      parent <- ggproto_parent(parent_layer, self)
+      data <- parent$map_statistic(data, plot)
+      names(self$stat$default_aes) <- old_nms
+      data
     },
     compute_position = function(self, data, layout) {
       data <- self$geom$.data_unmapper(data)
@@ -50,4 +68,18 @@ new_ggside_layer <- function(side = c("x","y"), geom = NULL, stat = NULL, data =
   )
 
 
+}
+
+drop_plot_aes <- function(plot_map, layer_map, side) {
+  #if layer mapping fill/colour variant exists
+  #remove it
+  p_nms <- names(plot_map)
+  l_nms <- names(layer_map)
+  if (paste0(side, "colour") %in% l_nms && any(to_drop <- p_nms %in% "colour"))
+    plot_map <- plot_map[!to_drop]
+
+  if (paste0(side, "fill") %in% l_nms && any(to_drop <- p_nms %in% "fill"))
+    plot_map <- plot_map[!to_drop]
+
+  plot_map
 }
