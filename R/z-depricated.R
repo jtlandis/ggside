0502ecc5f64094ee593f43d13812c05dd36b7637
jtlandis <jtlandis314@gmail.com>
2022-11-30 14:53:02 -0500

    Short deprecation desc

--- R/z-depricated.R
+++ R/z-depricated.R
@@ -1,5 +1,10 @@
 
 
-#' @rdname ggside-deprecated
+#' @name ggside-deprecated
+#' @title Deprecated Functions
+#'
+#' @description
+#' The following functions have been deprecated.
+#'
 #' @export
 as_ggsideFacet <- deprecated_fun(ggside_facet)
