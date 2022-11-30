4eb9c1ce3c8420aa50a48613a425cd041c250ace
jtlandis <jtlandis314@gmail.com>
2022-11-30 11:05:17 -0500

    better constructor function so that draw_panel's formals are correct.

--- R/constructor-.R
+++ R/constructor-.R
@@ -13,6 +13,20 @@ ggside_geom <- function(class_name = NULL,
   def_aes <- names(geom$default_aes)
   all_aes <- c(req_aes, opt_aes, non_mis, def_aes)
   .aes_to_map <- all_aes[all_aes %in% .ggside_global[[sprintf(".%s_aes",o)]]]
+
+  args <- formals(environment(geom$draw_panel)$f)
+  args2 <- lapply(names(args), as.name)
+  names(args2) <- names(args)
+  args2 <- args2[setdiff(names(args2), "self")]
+  fun <- function() {}
+  formals(fun) <- args
+  body(fun) <- expr({
+    data <- use_side_aes(data, side)
+    data <- self$.data_unmapper(data)
+    parent <- ggproto_parent(geom, self)
+    parent$draw_panel(!!!args2)
+  })
+
   ggplot2::ggproto(
     class_name,
     geom,
@@ -21,20 +35,12 @@ ggside_geom <- function(class_name = NULL,
     optional_aes = rename_side(geom$optional_aes, side),
     non_missing_aes = rename_side(geom$non_missing_aes, side),
     setup_data = function(self, data, params) {
+      browser()
       data <- parse_side_aes(data, params)
       data <- self$.data_unmapper(data)
       geom$setup_data(data, params)
     },
-    draw_panel = function(self, data, panel_params,
-                          coord, width = NULL, flipped_aes = FALSE) {
-      browser()
-      data <- use_side_aes(data, side)
-      data <- self$.data_unmapper(data)
-      parent <- ggplot2::ggproto_parent(geom, self)
-      parent$draw_panel(data = data, panel_params = panel_params,
-                        coord = coord, width = width,
-                        flipped_aes = flipped_aes)
-    },
+    draw_panel = fun,
     draw_key = function(data, params, size) {
       data <- use_side_aes(data, side)
       geom$draw_key(data = data, params = params, size = size)
@@ -60,23 +66,57 @@ ggside_geom <- function(class_name = NULL,
 #' Modifies the `$add` method such that
 #' when a positional scale is added it is
 #' recast with `new_pos_scale`
-ggside_scales <- function(scales) {
+ggside_scales <- function(scales, ggside) {
   new_scales <- ggproto(
     NULL,
     scales,
+    ggside = ggside,
     add = function(self, scale) {
-      ggproto_parent(scales, self)$add(scale)
-      if (!is.null(scale) && any(pos <- c("x","y") %in% scale$aesthetics)) {
-        side <- switch(c("x","y")[pos], x = "yside", y = "xside")
-        s <- self$scales[[self$n()]]
-        s$aesthetics <- c(s$aesthetics, paste0(side, s$aesthetics))
-        self$scales[[self$n()]] <- new_pos_scale(s)
+      parent <- ggproto_parent(scales, self)
+      parent$add(scale)
+      if (!is.null(scale)) {
+        if (any(pos <- c("x","y") %in% scale$aesthetics)) {
+          side <- switch(c("x","y")[pos], x = "yside", y = "xside")
+          s <- new_pos_scale(self$scales[[self$n()]])
+          self$scales[[self$n()]] <- s
+        } else if (any(pos <- c("ysidex", "xsidey") %in% scale$aesthetics)) {
+          side <- c("ysidex", "xsidey")[pos]
+          main_scale <- self$find(switch(side, ysidex = "x", xsidey = "y"))
+          # if main scale is already specified
+          # then assume its position.
+          if (any(main_scale)) {
+            main_scale <- self$scales[main_scale]
+            scale$position <- main_scale[[1]]$position
+          }
+          self$ggside[[side]] <- scale
+        }
       }
+    },
+    input = function(self) {
+      out <- lapply(self$scales, `[[`, "aesthetics")
+      x_scales <- self$find("x")
+      if (any(x_scales))
+        out[x_scales] <- lapply(out[x_scales], function(x) x[!grepl("yside", x)])
+      y_scales <- self$find("y")
+      if (any(y_scales))
+        out[y_scales] <- lapply(out[y_scales], function(x) x[!grepl("xside", x)])
+      unlist(out)
+    },
+    find = function(self, aesthetic) {
+      vapply(self$scales,
+             function(x) {
+               aes <- x$aesthetics
+               if(any(pos <- c("x","y") %in% aes)) {
+                 side <- switch(c("x","y")[pos], x = "yside", y = "xside")
+                 aes <- aes[!grepl(side, aes)]
+               }
+               any(aesthetic %in% aes)
+             }, logical(1))
     })
 
   lgl <- new_scales$find(c("x","y"))
   if (any(lgl))
-    new_scales[lgl] <- lapply(new_scales$scales[lgl], new_pos_scale)
+    new_scales$scales[lgl] <- lapply(new_scales$scales[lgl], new_pos_scale)
 
   new_scales
 
@@ -98,3 +138,7 @@ new_pos_scale <- function(scale) {
     })
 }
 
+
+fun_method <- function(name, args, fun) {
+
+}
