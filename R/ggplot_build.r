f4c89d3b8a9a980aa0db35eb9f846b61a06b8acb
jtlandis <jtlandis314@gmail.com>
2023-12-31 16:35:02 -0500

    this clone method is a bit more safe in terms of inheritance

--- R/ggplot_build.r
+++ R/ggplot_build.r
@@ -1,22 +1,32 @@
 
 clone_ggside <- function(ggside) {
-  new_ggside <- ggproto(NULL, ggside)
+  new_ggside <- ggside(x.pos = ggside$x.pos,
+                       y.pos = ggside$y.pos,
+                       scales = ggside$scales,
+                       collapse = ggside$collapse,
+                       draw_x_on = ggside$draw_x_on,
+                       draw_y_on = ggside$draw_y_on,
+                       strip = ggside$strip,
+                       respect_side_labels = ggside$respect_side_labels)
   if (!is.null(ggside$xsidey)) {
     new_ggside$xsidey <- ggside$xsidey$clone()
   }
   if (!is.null(ggside$ysidex)) {
     new_ggside$ysidex <- ggside$ysidex$clone()
   }
+  if (!is.null(ggside$sides_used)) {
+    new_ggside$sides_used <- ggside$sides_used
+  }
   new_ggside
 }
 
-ggside_guides <- function(guide) {
-  ggproto(
-    NULL,
-    guide,
-    build = mod_ggproto_fun(guide$build) |> browse_fun()
-  )
-}
+# ggside_guides <- function(guide) {
+#   ggproto(
+#     NULL,
+#     guide,
+#     build = mod_ggproto_fun(guide$build) |> browse_fun()
+#   )
+# }
 
 #' @export
 ggplot_build.ggside <- function(plot) {
