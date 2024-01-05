596f90edbdfe2e1f474d95742d3187d385a1d1ee
jtlandis <jtlandis314@gmail.com>
2024-01-05 15:45:35 -0500

    fixing bug with include script removing indents

--- R/constructor-2.R
+++ R/constructor-2.R
@@ -4,136 +4,137 @@ NULL
 ### INCLUDE END
 
 aes_to_map <- function(ggproto, side) {
-resolve_arg(side, c("x", "y"), null.ok = FALSE)
-other_side <- switch(side, x = "y", y = "x")
-req_aes <- pull_aes(ggproto$required_aes)
-opt_aes <- pull_aes(ggproto$optional_aes)
-non_mis <- pull_aes(ggproto$non_missing_aes)
-def_aes <- names(ggproto$default_aes)
-all_aes <- unique(c(req_aes, opt_aes, non_mis, def_aes))
-all_aes[all_aes %in% .ggside_global[[sprintf(".%s_aes", other_side)]]]
+  resolve_arg(side, c("x", "y"), null.ok = FALSE)
+  other_side <- switch(side, x = "y", y = "x")
+  req_aes <- pull_aes(ggproto$required_aes)
+  opt_aes <- pull_aes(ggproto$optional_aes)
+  non_mis <- pull_aes(ggproto$non_missing_aes)
+  def_aes <- names(ggproto$default_aes)
+  all_aes <- unique(c(req_aes, opt_aes, non_mis, def_aes))
+  all_aes[all_aes %in% .ggside_global[[sprintf(".%s_aes", other_side)]]]
 }
 
 data_unmap <- function(data, side) {
-names(data) <- sub(sprintf("%sside", side), "", names(data))
-data
+  names(data) <- sub(sprintf("%sside", side), "", names(data))
+  data
 }
 
 data_map <- function(data, side, map) {
-x <- names(data)
-aes <- x %in% map
-x[aes] <- sprintf("%sside%s", side, x[aes])
-names(data) <- x
-data
+  x <- names(data)
+  aes <- x %in% map
+  x[aes] <- sprintf("%sside%s", side, x[aes])
+  names(data) <- x
+  data
 }
 
 enforce_geom <- function(geom) {
-stopifnot("`geom` should be a ggproto object"=inherits(geom, "ggproto"),
-"`geom` should be a Geom object"=inherits(geom, "Geom"))
-class_name <- class(geom)[1]
-where <- find(class_name, simple.words = FALSE)
-if (length(where)==0) {
-stop(
-sprintf("could not find ggproto Geom <%s>", class_name)
-)
-}
-if (length(where)>1) {
-cli::cli_abort("found {length(where)} locations for {.arg {class_name}}: {where}")
-}
-class_name <- as.name(class_name)
-if (grepl("^package:", where)) {
-pkg <- as.name(sub("package:", "", where))
-expr <- call("::", pkg, class_name)
-} else {
-expr <- class_name
+  stopifnot(
+    "`geom` should be a ggproto object" = inherits(geom, "ggproto"),
+    "`geom` should be a Geom object" = inherits(geom, "Geom")
+  )
+  class_name <- class(geom)[1]
+  where <- find(class_name, simple.words = FALSE)
+  if (length(where) == 0) {
+    stop(sprintf("could not find ggproto Geom <%s>", class_name))
+  }
+  if (length(where) > 1) {
+    cli::cli_abort("found {length(where)} locations for {.arg {class_name}}: {where}")
+  }
+  class_name <- as.name(class_name)
+  if (grepl("^package:", where)) {
+    pkg <- as.name(sub("package:", "", where))
+    expr <- call("::", pkg, class_name)
+  } else {
+    expr <- class_name
+  }
+
+  #finally check that it is identical
+  stopifnot("Could not confirm geom" = identical(geom, eval(expr)))
+  expr
+
 }
 
-#finally check that it is identical
-stopifnot("Could not confirm geom"=identical(geom, eval(expr)))
-expr
+ggside_geom_setup_data <-
+  function(geom, side, env = parent.frame()) {
+    ggprotoGeom <- eval(geom, envir = env)
+    args <- ggproto_formals0(ggprotoGeom$setup_data)
+    #all ggplot2 geoms do NOT have `self`,
+    # but in case that changes in the future
+    # or another geom is extended that does have `self`
+    has_self <- !is.null(args[["self"]]) || "self" %in% names(args)
+    body <- if (has_self) {
+      args <- args[setdiff(names(args), "self")]
+      expr({
+        data <- parse_side_aes(data)
+        data <- data_unmap(data, !!side)
+        parent <- ggproto_parent(!!geom, self)
+        parent$setup_data(!!!args)
+      })
+    } else {
+      expr({
+        data <- parse_side_aes(data)
+        data <- data_unmap(data, !!side)
+        (!!geom)$setup_data(!!!args)
+      })
+    }
+    rlang::new_function(
+      args = ggproto_formals(ggprotoGeom$setup_data),
+      body = body,
+      env = env
+    )
+  }
 
-}
-
-ggside_geom_setup_data <- function(geom, side, env = parent.frame()) {
-ggprotoGeom <- eval(geom, envir = env)
-args <- ggproto_formals0(ggprotoGeom$setup_data)
-#all ggplot2 geoms do NOT have `self`,
-# but in case that changes in the future
-# or another geom is extended that does have `self`
-has_self <- !is.null(args[["self"]]) || "self" %in% names(args)
-body <- if(has_self) {
-args <- args[setdiff(names(args), "self")]
-expr({
-data <- parse_side_aes(data)
-data <- data_unmap(data, !!side)
-parent <- ggproto_parent(!!geom, self)
-parent$setup_data(!!!args)
-})
-} else {
-expr({
-data <- parse_side_aes(data)
-data <- data_unmap(data, !!side)
-(!!geom)$setup_data(!!!args)
-})
-}
-rlang::new_function(
-args = ggproto_formals(ggprotoGeom$setup_data),
-body = body,
-env = env
-)
-}
-
-ggside_geom_draw_panel <- function(geom, side, env = parent.frame()) {
-
-ggprotoGeom <- eval(geom, envir = env)
-args <- ggproto_formals0(ggprotoGeom$draw_panel)
-has_self <- !is.null(args[["self"]]) || "self" %in% names(args)
-body <- if(has_self) {
-args <- args[!names(args) %in% "self"]
-expr({
-data <- use_side_aes(data, !!side)
-data <- data_unmap(data, !!side)
-parent <- ggproto_parent(!!geom, self)
-parent$draw_panel(!!!args)
-})
-} else {
-expr({
-data <- use_side_aes(data, !!side)
-data <- data_unmap(data, !!side)
-(!!geom)$draw_panel(!!!args)
-})
-}
-rlang::new_function(
-args = ggproto_formals(ggprotoGeom$draw_panel),
-body = body,
-env = env
-)
-}
+ggside_geom_draw_panel <-
+  function(geom, side, env = parent.frame()) {
+    ggprotoGeom <- eval(geom, envir = env)
+    args <- ggproto_formals0(ggprotoGeom$draw_panel)
+    has_self <- !is.null(args[["self"]]) || "self" %in% names(args)
+    body <- if (has_self) {
+      args <- args[!names(args) %in% "self"]
+      expr({
+        data <- use_side_aes(data, !!side)
+        data <- data_unmap(data, !!side)
+        parent <- ggproto_parent(!!geom, self)
+        parent$draw_panel(!!!args)
+      })
+    } else {
+      expr({
+        data <- use_side_aes(data, !!side)
+        data <- data_unmap(data, !!side)
+        (!!geom)$draw_panel(!!!args)
+      })
+    }
+    rlang::new_function(
+      args = ggproto_formals(ggprotoGeom$draw_panel),
+      body = body,
+      env = env
+    )
+  }
 
 ggside_geom_draw_key <- function(geom, side, env = parent.frame()) {
-ggprotoGeom <- eval(geom, envir = env)
-args <- ggproto_formals0(ggprotoGeom$draw_key)
-has_self <- !is.null(args[["self"]]) || "self" %in% names(args)
-body <- if(has_self) {
-args <- args[setdiff(names(args), "self")]
-expr({
-data <- use_side_aes(data, !!side)
-data <- data_unmap(data, !!side)
-parent <- ggproto_parent(!!geom, self)
-parent$draw_key(!!!args)
-})
-} else {
-expr({
-data <- use_side_aes(data, !!side)
-data <- data_unmap(data, !!side)
-(!!geom)$draw_key(!!!args)
-})
-}
-rlang::new_function(
-args = ggproto_formals(ggprotoGeom$draw_key),
-body = body,
-env = env
-)
+  ggprotoGeom <- eval(geom, envir = env)
+  args <- ggproto_formals0(ggprotoGeom$draw_key)
+  has_self <- !is.null(args[["self"]]) || "self" %in% names(args)
+  body <- if (has_self) {
+    args <- args[setdiff(names(args), "self")]
+    expr({
+      data <- use_side_aes(data, !!side)
+      data <- data_unmap(data, !!side)
+      parent <- ggproto_parent(!!geom, self)
+      parent$draw_key(!!!args)
+    })
+  } else {
+    expr({
+      data <- use_side_aes(data, !!side)
+      data <- data_unmap(data, !!side)
+      (!!geom)$draw_key(!!!args)
+    })
+  }
+  rlang::new_function(
+    args = ggproto_formals(ggprotoGeom$draw_key),
+    body = body,
+    env = env
+  )
 }
 
 #' @name ggside_geom
@@ -145,70 +146,69 @@ env = env
 #' @param ... additional members to add to the ggproto class.
 #' @export
 ggside_geom <- function(class_name = NULL,
-geom = NULL,
-side = NULL,
-...) {
-side <- resolve_arg(side, c("x", "y"), null.ok = FALSE)
+                        geom = NULL,
+                        side = NULL,
+                        ...) {
+  side <- resolve_arg(side, c("x", "y"), null.ok = FALSE)
 
-members <- try_fetch(
-dots_list(
-...,
-default_aes = new_default_aes(geom, side),
-required_aes = rename_side(geom$required_aes, side),
-optional_aes = rename_side(geom$optional_aes, side),
-non_missing_aes = rename_side(geom$non_missing_aes, side),
-draw_key = new_ggproto_fun(
-geom$draw_key,
-{
-data <- use_side_aes(data, !!side)
-data <- data_unmap(data, !!side)
-call_parent_method
-}),
-.homonyms = "error"
-),
-error = function(cnd) {
-reserved_args <- str_extr(cnd$body, "`[^`]+`")
-len <- length(reserved_args)
-cli::cli_abort("{cli::qty(len)} argument{?s} {reserved_args} {?is/are} reserved and cannot be specified in `...`",
-parent = cnd)
-}
-)
-ggplot2::ggproto(
-class_name,
-geom,
-!!!members
-)
+  members <- try_fetch(
+    dots_list(
+      ...,
+      default_aes = new_default_aes(geom, side),
+      required_aes = rename_side(geom$required_aes, side),
+      optional_aes = rename_side(geom$optional_aes, side),
+      non_missing_aes = rename_side(geom$non_missing_aes, side),
+      draw_key = new_ggproto_fun(geom$draw_key,
+                                 {
+                                   data <- use_side_aes(data, !!side)
+                                   data <- data_unmap(data, !!side)
+                                   call_parent_method
+                                 }),
+      .homonyms = "error"
+    ),
+    error = function(cnd) {
+      reserved_args <- str_extr(cnd$body, "`[^`]+`")
+      len <- length(reserved_args)
+      cli::cli_abort(
+        "{cli::qty(len)} argument{?s} {reserved_args} {?is/are} reserved and cannot be specified in `...`",
+        parent = cnd
+      )
+    }
+  )
+  ggplot2::ggproto(class_name,
+                   geom,
+                   !!!members)
 }
 
 ggside_stat <- function(class_name = NULL,
-stat = NULL,
-side = NULL,
-...) {
-side <- resolve_arg(side, c("x", "y"), null.ok = FALSE)
-mapping <- stat$default_aes
-names(mapping) <- rename_side(names(mapping), side)
-members <- try_fetch(
-dots_list(
-...,
-default_aes = mapping,
-required_aes = rename_side(stat$required_aes, side),
-optional_aes = rename_side(stat$optional_aes, side),
-non_missing_aes = rename_side(stat$non_missing_aes, side),
-.homonyms = "error"
-),
-error = function(cnd) {
-reserved_args <- str_extr(cnd$body, "`[^`]+`")
-len <- length(reserved_args)
-cli::cli_abort("{cli::qty(len)} argument{?s} {reserved_args} {?is/are} reserved and cannot be specified in `...`",
-parent = cnd)
-}
-)
+                        stat = NULL,
+                        side = NULL,
+                        ...) {
+  side <- resolve_arg(side, c("x", "y"), null.ok = FALSE)
+  mapping <- stat$default_aes
+  names(mapping) <- rename_side(names(mapping), side)
+  members <- try_fetch(
+    dots_list(
+      ...,
+      default_aes = mapping,
+      required_aes = rename_side(stat$required_aes, side),
+      optional_aes = rename_side(stat$optional_aes, side),
+      non_missing_aes = rename_side(stat$non_missing_aes, side),
+      .homonyms = "error"
+    ),
+    error = function(cnd) {
+      reserved_args <- str_extr(cnd$body, "`[^`]+`")
+      len <- length(reserved_args)
+      cli::cli_abort(
+        "{cli::qty(len)} argument{?s} {reserved_args} {?is/are} reserved and cannot be specified in `...`",
+        parent = cnd
+      )
+    }
+  )
 
-ggplot2::ggproto(
-class_name,
-stat,
-!!!members
-)
+  ggplot2::ggproto(class_name,
+                   stat,
+                   !!!members)
 }
 
 
@@ -216,112 +216,128 @@ stat,
 # calls source function first to make the layer.
 #
 #' @export
-ggside_layer_function <- function(fun, side = NULL, env = caller_env(), ...,
-force_missing) {
-# browser()
-resolve_arg(side, c("x", "y"), null.ok = FALSE)
-fun_sym <- caller_arg(fun)
-formals_ <- formals(fun)
-formals_ <- dots_list(!!!formals_, ..., .homonyms = "last")
-formals_ <- formals_[!vapply(formals_, is_zap, logical(1))]
-pull_mapping <- expr(mapping <- layer$mapping)
-has_orientation <- !is.null(formals_[["orientation"]]) || "orientation" %in% names(formals_)
-if (has_orientation) {
-if (is.na(formals_[["orientation"]]))
-formals_[["orientation"]] <- side
+ggside_layer_function <-
+  function(fun,
+           side = NULL,
+           env = caller_env(),
+           ...,
+           force_missing) {
+    # browser()
+    resolve_arg(side, c("x", "y"), null.ok = FALSE)
+    fun_sym <- caller_arg(fun)
+    formals_ <- formals(fun)
+    formals_ <- dots_list(!!!formals_, ..., .homonyms = "last")
+    formals_ <- formals_[!vapply(formals_, is_zap, logical(1))]
+    pull_mapping <- expr(mapping <- layer$mapping)
+    has_orientation <-
+      !is.null(formals_[["orientation"]]) ||
+      "orientation" %in% names(formals_)
+    if (has_orientation) {
+      if (is.na(formals_[["orientation"]]))
+        formals_[["orientation"]] <- side
 
-pull_mapping <- expr(mapping <- default_stat_aes(layer$mapping, layer$stat, orientation))
-}
-defaults_ <- formals_as_defaults(formals_)
-if (!missing(force_missing)) {
-defaults_ <- defaults_[!names(defaults_) %in% force_missing]
-}
-non_pos_aes <- paste0(side, c("fill", "colour", "color"))
-pos_aes <- paste0(side, "side")
-`_class` <- switch(side, x = "XLayer", y = "YLayer")
-Side <- switch(side, x = "\\1Xside\\L\\2", y = "\\1Yside\\L\\2")
-body <- expr({
+      pull_mapping <-
+        expr(mapping <-
+               default_stat_aes(layer$mapping, layer$stat, orientation))
+    }
+    defaults_ <- formals_as_defaults(formals_)
+    if (!missing(force_missing)) {
+      defaults_ <- defaults_[!names(defaults_) %in% force_missing]
+    }
+    non_pos_aes <- paste0(side, c("fill", "colour", "color"))
+    pos_aes <- paste0(side, "side")
+    `_class` <- switch(side, x = "XLayer", y = "YLayer")
+    Side <- switch(side, x = "\\1Xside\\L\\2", y = "\\1Yside\\L\\2")
+    body <- expr({
+      env <- caller_env()
+      map_names <- names(mapping)
+      non_pos_aes <- !!non_pos_aes
+      side_aes_used <- map_names %in% non_pos_aes
+      # temporarily remove ggside specific aes
+      # need to re-add afterwards
+      if (any(side_aes_used)) {
+        names(mapping)[side_aes_used] <-
+          sub(!!side, "", map_names[side_aes_used])
+      }
+      pos_aes_used <- grepl(!!pos_aes, map_names, fixed = TRUE)
+      if (any(pos_aes_used)) {
+        names(mapping)[pos_aes_used] <-
+          sub(pos_aes, "", map_names[pos_aes_used])
+      }
+      to_zap <- non_pos_aes[non_pos_aes %in% ...names()]
+      layer <- call_layer_param_aware((!!fun_sym)(!!!defaults_),
+                                      zap = to_zap,
+                                      ...,
+                                      env = env)
+      !!pull_mapping
+      # re-add after vanilla layer has been constructed
+      # mapping <- layer$mapping
+      if (any(side_aes_used)) {
+        names(mapping)[side_aes_used] <- map_names[side_aes_used]
+      }
 
-env <- caller_env()
-map_names <- names(mapping)
-non_pos_aes <- !!non_pos_aes
-side_aes_used <- map_names %in% non_pos_aes
-# temporarily remove ggside specific aes
-# need to re-add afterwards
-if (any(side_aes_used)) {
-names(mapping)[side_aes_used] <- sub(!!side, "", map_names[side_aes_used])
-}
-pos_aes_used <- grepl(!!pos_aes, map_names, fixed = TRUE)
-if (any(pos_aes_used)) {
-names(mapping)[pos_aes_used] <- sub(pos_aes, "", map_names[pos_aes_used])
-}
-to_zap <- non_pos_aes[non_pos_aes %in% ...names()]
-layer <- call_layer_param_aware(
-(!!fun_sym)(!!!defaults_),
-zap = to_zap,
-...,
-env = env)
-!!pull_mapping
-# re-add after vanilla layer has been constructed
-# mapping <- layer$mapping
-if (any(side_aes_used)) {
-names(mapping)[side_aes_used] <- map_names[side_aes_used]
-}
+      names(mapping) <- rename_side(names(mapping), !!side)
 
-names(mapping) <- rename_side(names(mapping), !!side)
+      # remaps
+      geom_aes_map <- aes_to_map(layer$geom, !!side)
+      stat_aes_map <- aes_to_map(layer$stat, !!side)
+      remap <- union(geom_aes_map, stat_aes_map)
+      # ggside_geom
+      geom <- ggside_geom2(
+        gsub("(Geom)([A-Z])", !!Side,
+             class(layer$geom)[1], perl = T),
+        geom = layer$geom,
+        side = !!side
+      )
+      stat <- ggside_stat(
+        gsub("(Stat)([A-Z])", !!Side,
+             class(layer$stat)[1], perl = T),
+        stat = layer$stat,
+        side = !!side
+      )
+      SideLayer <- ggproto(
+        !!`_class`,
+        layer,
+        geom = geom,
+        stat = stat,
+        mapping = mapping
+      )
+      new_ggside_layer2(SideLayer, !!side, remap)
+    })
 
-# remaps
-geom_aes_map <- aes_to_map(layer$geom, !!side)
-stat_aes_map <- aes_to_map(layer$stat, !!side)
-remap <- union(geom_aes_map, stat_aes_map)
-# ggside_geom
-geom <- ggside_geom2(gsub("(Geom)([A-Z])", !!Side,
-class(layer$geom)[1], perl = T),
-geom = layer$geom,
-side = !!side)
-stat <- ggside_stat(gsub("(Stat)([A-Z])", !!Side,
-class(layer$stat)[1], perl = T),
-stat = layer$stat,
-side = !!side)
-SideLayer <- ggproto(
-!!`_class`, layer,
-geom = geom, stat = stat,
-mapping = mapping
-)
-new_ggside_layer2(SideLayer, !!side, remap)
-})
+    new_function(formals_,
+                 body = body,
+                 env = env)
 
-new_function(
-formals_,
-body = body,
-env = env
-)
-
-}
+  }
 
 zap_dots <- function(call, zap = character(), ...) {
-any_zap <- length(zap) > 0
-if (any_zap) {
-to_zap <- rep_named(zap, list(zap()))
-dots <- enexprs(...)
-dots_zapped <- dots[!names(dots) %in% zap]
-call <- call_modify(call, ...=zap(), !!! dots_zapped)
+  any_zap <- length(zap) > 0
+  if (any_zap) {
+    to_zap <- rep_named(zap, list(zap()))
+    dots <- enexprs(...)
+    dots_zapped <- dots[!names(dots) %in% zap]
+    call <- call_modify(call, ... = zap(), !!!dots_zapped)
+  }
+  call
 }
-call
-}
-call_layer_param_aware <- function(expr, zap = character(), ..., env = caller_env()) {
-call <- match.call()$expr
-call <- zap_dots(call, zap = zap, ...)
-layer <- eval(call, envir = env)
-any_zap <- length(zap)>0
-dot_names <- ...names()
-if (length(intersect(dot_names, zap))>0) {
-ind <- which(dot_names %in% zap)
-new_name <- sub("color", "colour", dot_names[ind], fixed = TRUE)
-lst <- vector("list", length(ind))
-for (i in seq_along(lst))
-lst[[i]] <- ...elt(ind[i])
-layer$aes_params[new_name] <- lst
-}
-layer
-}
+call_layer_param_aware <-
+  function(expr,
+           zap = character(),
+           ...,
+           env = caller_env()) {
+    call <- match.call()$expr
+    call <- zap_dots(call, zap = zap, ...)
+    layer <- eval(call, envir = env)
+    any_zap <- length(zap) > 0
+    dot_names <- ...names()
+    if (length(intersect(dot_names, zap)) > 0) {
+      ind <- which(dot_names %in% zap)
+      new_name <- sub("color", "colour", dot_names[ind], fixed = TRUE)
+      lst <- vector("list", length(ind))
+      for (i in seq_along(lst))
+        lst[[i]] <- ...elt(ind[i])
+      layer$aes_params[new_name] <- lst
+    }
+    layer
+  }
--- R/side-facet_.R
+++ R/side-facet_.R
@@ -12,145 +12,152 @@ NULL
 #' @param facet Facet ggproto Object to replace
 #' @param ggside ggside object to update
 #' @export
-ggside_facet <- function(facet, ggside) UseMethod("ggside_facet", facet)
+ggside_facet <-
+  function(facet, ggside)
+    UseMethod("ggside_facet", facet)
 
 ggside_facet.default <- function(facet, ggside = ggside()) {
-abort(
-sprintf("No %s() method for object of class <%s>", "ggside_facet", class(facet)[1]),
-)
+  abort(sprintf(
+    "No %s() method for object of class <%s>",
+    "ggside_facet",
+    class(facet)[1]
+  ),)
 }
 
 ggside_facet.ggsideFacet <- function(facet, ggside = ggside()) {
-param <- facet$params
-param$ggside <- ggside
-check_facet(
-ggproto(NULL, facet, params = param)
-)
+  param <- facet$params
+  param$ggside <- ggside
+  check_facet(ggproto(NULL, facet, params = param))
 }
 
 ggside_facet.FacetNull <- function(facet, ggside = ggside()) {
-
-new_facet <- new_ggside_facet(facet, ggside)
-check_facet(
-ggproto(
-"FacetSideNull",
-new_facet,
-draw_panels = sideFacetNull_draw_panels,
-map_data = sideFacetNull_map_data
-)
-)
+  new_facet <- new_ggside_facet(facet, ggside)
+  check_facet(
+    ggproto(
+      "FacetSideNull",
+      new_facet,
+      draw_panels = sideFacetNull_draw_panels,
+      map_data = sideFacetNull_map_data
+    )
+  )
 
 }
 
 ggside_facet.FacetGrid <- function(facet, ggside = ggside()) {
-
-new_facet <- new_ggside_facet(facet, ggside)
-check_facet(
-ggproto(
-"FacetSideGrid",
-new_facet,
-draw_panels = sideFacetGrid_draw_panels,
-map_data = sideFacetGrid_map_data
-)
-)
+  new_facet <- new_ggside_facet(facet, ggside)
+  check_facet(
+    ggproto(
+      "FacetSideGrid",
+      new_facet,
+      draw_panels = sideFacetGrid_draw_panels,
+      map_data = sideFacetGrid_map_data
+    )
+  )
 
 }
 
 ggside_facet.FacetWrap <- function(facet, ggside = ggside()) {
-
-new_facet <- new_ggside_facet(facet, ggside)
-check_facet(
-ggproto(
-"FacetSideWrap",
-new_facet,
-draw_panels = sideFacetWrap_draw_panels,
-map_data = sideFacetWrap_map_data
-)
-)
+  new_facet <- new_ggside_facet(facet, ggside)
+  check_facet(
+    ggproto(
+      "FacetSideWrap",
+      new_facet,
+      draw_panels = sideFacetWrap_draw_panels,
+      map_data = sideFacetWrap_map_data
+    )
+  )
 }
 
 new_ggside_facet <- function(facet, ggside) {
-force(facet)
-params <- facet$params
-params[["ggside"]] <- ggside
-ggproto(
-"ggsideFacet",
-facet,
-params = params,
-compute_layout = ggside_compute_layout(facet),
-train_scales = mod_ggproto_fun(
-facet$train_scales,
-x_scales[[1]]$aesthetics ~ unique(unlist(lapply(x_scales, `[[`, "aesthetics"))),
-y_scales[[1]]$aesthetics ~ unique(unlist(lapply(y_scales, `[[`, "aesthetics")))
-),
-finish_data = new_ggproto_fun(
-facet$finish_data,
-{
-if ("PANEL_TYPE" %in% names(data) &&
-all(data$PANEL_TYPE != "main")) {
-data <- use_side_aes(data, unique(data$PANEL_TYPE))
-}
-call_parent_method
-})
-)
+  force(facet)
+  params <- facet$params
+  params[["ggside"]] <- ggside
+  ggproto(
+    "ggsideFacet",
+    facet,
+    params = params,
+    compute_layout = ggside_compute_layout(facet),
+    train_scales = mod_ggproto_fun(
+      facet$train_scales,
+      x_scales[[1]]$aesthetics ~ unique(unlist(lapply(
+        x_scales, `[[`, "aesthetics"
+      ))),
+      y_scales[[1]]$aesthetics ~ unique(unlist(lapply(
+        y_scales, `[[`, "aesthetics"
+      )))
+    ),
+    finish_data = new_ggproto_fun(facet$finish_data,
+                                  {
+                                    if ("PANEL_TYPE" %in% names(data) &&
+                                        all(data$PANEL_TYPE != "main")) {
+                                      data <- use_side_aes(data, unique(data$PANEL_TYPE))
+                                    }
+                                    call_parent_method
+                                  })
+  )
 }
 
 
 ggside_compute_layout <- function(facet) {
-force(facet)
-function(data, params){
-layout <- facet$compute_layout(data, params)
-layout <- check_scales_collapse(layout, params)
-layout <- sidePanelLayout(layout, ggside = params$ggside)
-layout
-}
+  force(facet)
+  function(data, params) {
+    layout <- facet$compute_layout(data, params)
+    layout <- check_scales_collapse(layout, params)
+    layout <- sidePanelLayout(layout, ggside = params$ggside)
+    layout
+  }
 }
 
 check_facet <- function(facet) {
-fp <- facet$params
-#this proto should be the same as the one on the plot
-ggside <- facet$ggside
-col <- ggside$collapse
-if (!is.null(fp$free) &&
-!is.null(col) &&
-inherits(facet, "FacetWrap") &&
-any(.lgl <- vapply(fp$free, identity, logical(1)))) {
-# if ggside collapse all - but scales is free - prioritize the scale and dont
-# collapse
-# i.e. facet_wrap(..., scales='free_y') + ggside(collapse="y") --> warning
-# main plots may have different y scales and thus we cannot collapse y.
-s <- sum(c(1,2)*.lgl)
-new_col <- switch(
-s,
-free_x = {
-.f <- "free_x"
-switch(col,
-all = "y",
-x = NULL,
-col)
-},
-free_y = {
-.f <- "free_y"
-switch(col,
-all = "x",
-y = NULL,
-col)
-},
-free = {
-.f <- "free"
-NULL
-}
-)
+  fp <- facet$params
+  #this proto should be the same as the one on the plot
+  ggside <- facet$ggside
+  col <- ggside$collapse
+  if (!is.null(fp$free) &&
+      !is.null(col) &&
+      inherits(facet, "FacetWrap") &&
+      any(.lgl <- vapply(fp$free, identity, logical(1)))) {
+    # if ggside collapse all - but scales is free - prioritize the scale and dont
+    # collapse
+    # i.e. facet_wrap(..., scales='free_y') + ggside(collapse="y") --> warning
+    # main plots may have different y scales and thus we cannot collapse y.
+    s <- sum(c(1, 2) * .lgl)
+    new_col <- switch(s,
+                      free_x = {
+                        .f <- "free_x"
+                        switch(col,
+                               all = "y",
+                               x = NULL,
+                               col)
+                      },
+                      free_y = {
+                        .f <- "free_y"
+                        switch(col,
+                               all = "x",
+                               y = NULL,
+                               col)
+                      },
+                      free = {
+                        .f <- "free"
+                        NULL
+                      })
 
-warning(glue("Plot's Facet parameter `scales = \"{.f}\"` is ",
-"incompatible with `ggside(..., collapse = \"{col}\")`.",
-" Setting collapse to ",
-if(is.null(new_col)) 'NULL' else glue('"{new_col}"')),
-call. = F)
-ggside$collapse <- new_col
+    warning(
+      glue(
+        "Plot's Facet parameter `scales = \"{.f}\"` is ",
+        "incompatible with `ggside(..., collapse = \"{col}\")`.",
+        " Setting collapse to ",
+        if (is.null(new_col))
+          'NULL'
+        else
+          glue('"{new_col}"')
+      ),
+      call. = F
+    )
+    ggside$collapse <- new_col
 
-}
-invisible(facet)
+  }
+  invisible(facet)
 }
 
 # ggside_train_scales <- function(facet) {
@@ -185,6 +192,3 @@ invisible(facet)
 # facet$train_scales(x_scales, y_scales, layout, data, params)
 # }
 # }
-
-
-
