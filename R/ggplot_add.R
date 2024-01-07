f5e9cbf893458597eba069e4ff009ef892412e14
jtlandis <jtlandis314@gmail.com>
2024-01-07 12:48:01 -0500

    redundant file

--- R/ggplot_add.R
+++ R/ggplot_add.R
@@ -1,5 +1,5 @@
 ### INCLUDE BEGIN
-#' @include ggplot_build.r
+#' @include ggside.R
 #' @include plot-construction.R
 NULL
 ### INCLUDE END
@@ -12,6 +12,29 @@ clone_ggside_plot <- function(plot) {
   plot
 
 }
+
+clone_ggside <- function(ggside) {
+  new_ggside <- ggside(x.pos = ggside$x.pos,
+                       y.pos = ggside$y.pos,
+                       scales = ggside$scales,
+                       collapse = ggside$collapse,
+                       draw_x_on = ggside$draw_x_on,
+                       draw_y_on = ggside$draw_y_on,
+                       strip = ggside$strip,
+                       respect_side_labels = ggside$respect_side_labels)
+  if (!is.null(ggside$xsidey)) {
+    new_ggside$xsidey <- ggside$xsidey$clone()
+  }
+  if (!is.null(ggside$ysidex)) {
+    new_ggside$ysidex <- ggside$ysidex$clone()
+  }
+  if (!is.null(ggside$sides_used)) {
+    new_ggside$sides_used <- ggside$sides_used
+  }
+  new_ggside
+}
+
+
 
 #' @importFrom ggplot2 ggplot_add
 #' @export
@@ -38,5 +61,6 @@ ggplot_add.ggside_scale <- function(object, plot, object_name){
   plot$scales$add(new_scale)
   as_ggside(plot)
 }
+
 
 
