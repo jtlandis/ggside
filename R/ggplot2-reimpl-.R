d7d4a3049b098f6bac00d276768f09c8ba7f74a3
jtlandis <jtlandis314@gmail.com>
2024-01-06 23:10:03 -0500

    moving functions that we keep from ggplot2 to here

---
+++ R/ggplot2-reimpl-.R
@@ -0,0 +1,19 @@
+
+
+# Check inputs with tibble but allow column vectors (see #2609 and #2374)
+as_gg_data_frame <- function(x) {
+  x <- lapply(x, validate_column_vec)
+  new_data_frame(x)
+}
+
+validate_column_vec <- function(x) {
+  if (is_column_vec(x)) {
+    dim(x) <- NULL
+  }
+  x
+}
+
+is_column_vec <- function(x) {
+  dims <- dim(x)
+  length(dims) == 2L && dims[[2]] == 1L
+}
