e539da75a21e2c6f070c938f9ebca47178e825d5
jtlandis <jtlandis314@gmail.com>
2024-02-28 11:37:31 -0500

    New tests for #58

---
+++ tests/testthat/test_ops_meaningful.R
@@ -0,0 +1,32 @@
+
+
+p <- mtcars |>
+  dplyr::mutate(cyl = as.factor(cyl)) |>
+  ggplot(aes(mpg, hp)) +
+  geom_point()
+
+
+
+testthat::test_that("non-mapping parameter works (alpha)", {
+  .addSide <- function(p, my_alpha) {
+    p + geom_ysideboxplot(
+      aes(x = cyl, y = hp, fill = cyl), orientation = "x",
+      alpha = my_alpha
+    )
+  }
+  vdiffr::expect_doppelganger("alpha-0.5", p + geom_ysideboxplot(
+    aes(x = cyl, y = hp, fill = cyl), orientation = "x",
+    alpha = 0.5
+  ))
+  vdiffr::expect_doppelganger("alpha-0.5-from-function", .addSide(p, 0.5))
+})
+
+
+testthat::test_that("No Ops meaningful warning", {
+
+  p2 <- p + geom_ysideboxplot(
+    aes(x = cyl, y = hp, fill = cyl), orientation = "x"
+  )
+  expect_no_warning(invisible(ggplot_build(p2)))
+
+})
