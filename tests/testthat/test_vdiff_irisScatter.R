2081f14ed0987f7bb2b1832b99b2b5e05318b961
jtlandis <jtlandis314@gmail.com>
2025-08-02 01:35:04 -0400

    style formatting

--- tests/testthat/test_vdiff_irisScatter.R
+++ tests/testthat/test_vdiff_irisScatter.R
@@ -1,5 +1,7 @@
 library(vdiffr)
-i2 <- within(iris, {Species2 <- rep(c("A","B"), 75)})
+i2 <- within(iris, {
+  Species2 <- rep(c("A", "B"), 75)
+})
 p <- ggplot(i2, aes(Sepal.Width, Sepal.Length, fill = Species)) +
   geom_point(aes(color = Species))
 
@@ -10,15 +12,16 @@ test_that("sidedensities plot correctly", {
   expect_doppelganger("Basic Side Density", p1)
   p2 <- p +
     geom_xsidedensity(aes(y = after_stat(density)), position = "stack") +
-    geom_ysidedensity(aes(x = after_stat(density), yfill = Species2), position = "stack")
+    geom_ysidedensity(aes(x = after_stat(density), yfill = Species2),
+      position = "stack"
+    )
   expect_doppelganger("Stacked Side Density", p2)
   p3 <- p2 +
     facet_grid(cols = vars(Species), rows = vars(Species2)) +
-    scale_yfill_manual(values = c("darkred","darkblue"))
+    scale_yfill_manual(values = c("darkred", "darkblue"))
   expect_doppelganger("FacetGrid Side Density", p3)
   p4 <- p3 + ggside(collapse = "all")
   expect_doppelganger("FacetGrid Collapsed Density", p4)
-
 })
 
 test_that("sidehistograms plot correctly", {
@@ -30,14 +33,14 @@ test_that("sidehistograms plot correctly", {
     geom_ysidehistogram(aes(x = after_stat(count), yfill = Species2), bins = 30)
   expect_doppelganger("yside histo", p2)
   p3 <- p2 +
-    geom_xsidehistogram(aes(xfill = Species, y = after_stat(count)), bins = 30) +
+    geom_xsidehistogram(aes(xfill = Species, y = after_stat(count)),
+      bins = 30
+    ) +
     facet_grid(cols = vars(Species), rows = vars(Species2)) +
-    scale_yfill_manual(values = c("darkred","darkblue")) +
+    scale_yfill_manual(values = c("darkred", "darkblue")) +
     guides(fill = "none")
   expect_doppelganger("FacetGrid histo", p3)
   p4 <- p3 + ggside(collapse = "all") +
-    scale_xfill_manual(values = c("tan2","gold","grey"))
+    scale_xfill_manual(values = c("tan2", "gold", "grey"))
   expect_doppelganger("Collapsed histo", p4)
 })
-
-
