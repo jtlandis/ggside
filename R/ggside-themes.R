ba0648b2c1249c81c22fdf321fb90e0b644e23ff
Justin Landis <justinlandis@Justins-MacBook-Air.local>
2025-08-01 20:28:50 -0400

    some style formatting

--- R/ggside-themes.R
+++ R/ggside-themes.R
@@ -1,12 +1,10 @@
-
-
 #' @rdname ggside-theme
 #' @export
 theme_ggside_grey <- function(base_size = 11,
                               base_family = "",
-                              base_line_size = base_size/22,
-                              base_rect_size = base_size/22) {
-  half_line <- base_size/2
+                              base_line_size = base_size / 22,
+                              base_rect_size = base_size / 22) {
+  half_line <- base_size / 2
 
   t <- theme(
     ggside.line = element_line(colour = "black", linewidth = base_line_size, linetype = 1, lineend = "butt", linejoin = "round"),
@@ -18,12 +16,12 @@ theme_ggside_grey <- function(base_size = 11,
     ),
     ggside.axis.line = element_blank(),
     ggside.axis.text = element_text(size = rel(0.8), colour = "grey30"),
-    ggside.axis.text.x = element_text(margin = margin(t = 0.8 * half_line/2), vjust = 1),
-    ggside.axis.text.x.top = element_text(margin = margin(b = 0.8 * half_line/2), vjust = 0),
-    ggside.axis.text.y = element_text(margin = margin(l = 0.8 * half_line/2), hjust = 1),
-    ggside.axis.text.y.right = element_text(margin = margin(l = 0.8 * half_line/2), hjust = 0),
+    ggside.axis.text.x = element_text(margin = margin(t = 0.8 * half_line / 2), vjust = 1),
+    ggside.axis.text.x.top = element_text(margin = margin(b = 0.8 * half_line / 2), vjust = 0),
+    ggside.axis.text.y = element_text(margin = margin(l = 0.8 * half_line / 2), hjust = 1),
+    ggside.axis.text.y.right = element_text(margin = margin(l = 0.8 * half_line / 2), hjust = 0),
     ggside.axis.ticks = element_line(colour = "grey20"),
-    ggside.axis.ticks.length = unit(half_line/2, "pt"),
+    ggside.axis.ticks.length = unit(half_line / 2, "pt"),
     ggside.panel.background = element_rect(fill = "grey92", colour = NA),
     ggside.panel.border = element_blank(),
     ggside.panel.grid = element_line(colour = "white"),
@@ -31,7 +29,6 @@ theme_ggside_grey <- function(base_size = 11,
   )
 
   t
-
 }
 
 
@@ -44,10 +41,12 @@ theme_ggside_gray <- theme_ggside_grey
 #' @export
 theme_ggside_bw <- function(base_size = 11,
                             base_family = "",
-                            base_line_size = base_size/22,
-                            base_rect_size = base_size/22) {
-  theme_ggside_grey(base_size = base_size, base_family = base_family,
-                    base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+                            base_line_size = base_size / 22,
+                            base_rect_size = base_size / 22) {
+  theme_ggside_grey(
+    base_size = base_size, base_family = base_family,
+    base_line_size = base_line_size, base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       ggside.panel.background = element_rect(fill = "white", color = NA),
       ggside.panel.border = element_rect(fill = NA, colour = "grey20"),
@@ -59,16 +58,18 @@ theme_ggside_bw <- function(base_size = 11,
 #' @rdname ggside-theme
 #' @export
 theme_ggside_linedraw <- function(base_size = 11,
-                            base_family = "",
-                            base_line_size = base_size/22,
-                            base_rect_size = base_size/22) {
-  half_line <- base_size/2
-  theme_ggside_bw(base_size = base_size, base_family = base_family,
-                    base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+                                  base_family = "",
+                                  base_line_size = base_size / 22,
+                                  base_rect_size = base_size / 22) {
+  half_line <- base_size / 2
+  theme_ggside_bw(
+    base_size = base_size, base_family = base_family,
+    base_line_size = base_line_size, base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       ggside.axis.text = element_text(colour = "black", size = rel(0.8)),
       ggside.axis.ticks = element_line(colour = "black", linewidth = rel(0.5)),
-      ggside.panel.border= element_rect(fill = NA, colour = "black", linewidth = rel(1)),
+      ggside.panel.border = element_rect(fill = NA, colour = "black", linewidth = rel(1)),
       ggside.panel.grid = element_line(colour = "black"),
       ggside.panel.grid.major = element_line(linewidth = rel(0.1)),
       ggside.panel.grid.minor = element_line(linewidth = rel(0.05))
@@ -78,12 +79,13 @@ theme_ggside_linedraw <- function(base_size = 11,
 #' @rdname ggside-theme
 #' @export
 theme_ggside_light <- function(base_size = 11,
-                                  base_family = "",
-                                  base_line_size = base_size/22,
-                                  base_rect_size = base_size/22) {
-
-  theme_ggside_grey(base_size = base_size, base_family = base_family,
-                  base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+                               base_family = "",
+                               base_line_size = base_size / 22,
+                               base_rect_size = base_size / 22) {
+  theme_ggside_grey(
+    base_size = base_size, base_family = base_family,
+    base_line_size = base_line_size, base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       ggside.panel.background = element_rect(fill = "white", colour = NA),
       ggside.panel.border = element_rect(fill = NA, colour = "grey70", linewidth = rel(1)),
@@ -97,12 +99,13 @@ theme_ggside_light <- function(base_size = 11,
 #' @rdname ggside-theme
 #' @export
 theme_ggside_dark <- function(base_size = 11,
-                                  base_family = "",
-                                  base_line_size = base_size/22,
-                                  base_rect_size = base_size/22) {
-
-  theme_ggside_grey(base_size = base_size, base_family = base_family,
-                  base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+                              base_family = "",
+                              base_line_size = base_size / 22,
+                              base_rect_size = base_size / 22) {
+  theme_ggside_grey(
+    base_size = base_size, base_family = base_family,
+    base_line_size = base_line_size, base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       ggside.panel.background = element_rect(fill = "grey50", colour = NA),
       ggside.panel.grid = element_line(colour = "grey42"),
@@ -110,18 +113,18 @@ theme_ggside_dark <- function(base_size = 11,
       ggside.panel.grid.minor = element_line(linewidth = rel(0.25)),
       ggside.axis.ticks = element_line(colour = "grey20", linewidth = rel(0.5))
     )
-
 }
 
 #' @rdname ggside-theme
 #' @export
 theme_ggside_minimal <- function(base_size = 11,
-                                  base_family = "",
-                                  base_line_size = base_size/22,
-                                  base_rect_size = base_size/22) {
-
-  theme_ggside_bw(base_size = base_size, base_family = base_family,
-                  base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+                                 base_family = "",
+                                 base_line_size = base_size / 22,
+                                 base_rect_size = base_size / 22) {
+  theme_ggside_bw(
+    base_size = base_size, base_family = base_family,
+    base_line_size = base_line_size, base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       ggside.axis.ticks = element_blank(),
       ggside.panel.border = element_blank()
@@ -132,35 +135,36 @@ theme_ggside_minimal <- function(base_size = 11,
 #' @export
 theme_ggside_classic <- function(base_size = 11,
                                  base_family = "",
-                                 base_line_size = base_size/22,
-                                 base_rect_size = base_size/22) {
-
-  theme_ggside_bw(base_size = base_size, base_family = base_family,
-                  base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+                                 base_line_size = base_size / 22,
+                                 base_rect_size = base_size / 22) {
+  theme_ggside_bw(
+    base_size = base_size, base_family = base_family,
+    base_line_size = base_line_size, base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       ggside.panel.border = element_blank(),
       ggside.panel.grid.major = element_blank(),
       ggside.panel.grid.minor = element_blank(),
       ggside.axis.line = element_line(colour = "black", linewidth = rel(1))
     )
-
 }
 
 #' @rdname ggside-theme
 #' @export
 theme_ggside_void <- function(base_size = 11,
-                                 base_family = "",
-                                 base_line_size = base_size/22,
-                                 base_rect_size = base_size/22) {
+                              base_family = "",
+                              base_line_size = base_size / 22,
+                              base_rect_size = base_size / 22) {
   t <- theme(
     ggside.line = element_blank(),
     ggside.rect = element_blank(),
-    ggside.text = element_text(family = base_family, face = "plain", colour = "black",
-                               size = base_size, lineheight = 0.9, hjust =0.5, vjust = 0.5,
-                               angle = 0, margin = margin(), debug = FALSE),
+    ggside.text = element_text(
+      family = base_family, face = "plain", colour = "black",
+      size = base_size, lineheight = 0.9, hjust = 0.5, vjust = 0.5,
+      angle = 0, margin = margin(), debug = FALSE
+    ),
     ggside.axis.text = element_blank(),
     ggside.axis.ticks.length = unit(0, "pt")
   )
   t
-
 }
--- R/side-facet-null.R
+++ R/side-facet-null.R
@@ -8,12 +8,11 @@ NULL
 
 sideFacetNull_draw_panels <- function(panels, layout, x_scales, y_scales,
                                       ranges, coord, data, theme, params) {
-
-
-  if (params$ggside$strip!="default") {
+  if (params$ggside$strip != "default") {
     warn("`ggside(strip = 'main', ...)` is only compatible with `facet_grid(...)`",
-         .frequency = "regularly",
-         .frequency_id = "ggside_strip_misuse_null")
+      .frequency = "regularly",
+      .frequency_id = "ggside_strip_misuse_null"
+    )
   }
 
   ncol <- max(layout$COL)
@@ -24,7 +23,7 @@ sideFacetNull_draw_panels <- function(panels, layout, x_scales, y_scales,
   panels <- panels[panel_order]
   panel_pos <- convertInd(layout$ROW, layout$COL, nrow)
   layout$panel_pos <- panel_pos
-  side_panels_present <- c("x","y")[c("x","y")%in%layout$PANEL_TYPE]
+  side_panels_present <- c("x", "y")[c("x", "y") %in% layout$PANEL_TYPE]
   x.pos <- params$ggside$x.pos
   y.pos <- params$ggside$y.pos
   ranges <- map_panel_type(ranges, layout$PANEL_TYPE)
@@ -34,7 +33,7 @@ sideFacetNull_draw_panels <- function(panels, layout, x_scales, y_scales,
   # ask the coordinate system if it wants to specify one
   aspect_ratio <- theme$aspect.ratio
   if (is.null(aspect_ratio)) {
-    aspect_ratio <- coord$aspect(ranges[[layout[layout$PANEL_TYPE=="main",]$PANEL[1L]]])
+    aspect_ratio <- coord$aspect(ranges[[layout[layout$PANEL_TYPE == "main", ]$PANEL[1L]]])
   }
 
   if (is.null(aspect_ratio)) {
@@ -43,53 +42,55 @@ sideFacetNull_draw_panels <- function(panels, layout, x_scales, y_scales,
   } else {
     respect <- TRUE
   }
-  #theme side panel scale
+  # theme side panel scale
+  # browser()
   side.panel.scale.x <- calc_element("ggside.panel.scale.x", theme)
   side.panel.scale.y <- calc_element("ggside.panel.scale.y", theme)
 
   empty_table <- matrix(list(zeroGrob()), nrow = nrow, ncol = ncol)
   panel_table <- empty_table
   panel_table[panel_pos] <- panels
-  empties <- apply(panel_table, c(1,2), function(x) is.zero(x[[1]]))
-  p.widths <- if("y"%in% side_panels_present) {
+  empties <- apply(panel_table, c(1, 2), function(x) is.zero(x[[1]]))
+  p.widths <- if ("y" %in% side_panels_present) {
     .widths <- c(1, side.panel.scale.y)
-    .tmp <- layout[layout[["PANEL_TYPE"]]=="y",][["COL"]]
-    if(y.pos=="left"){
+    .tmp <- layout[layout[["PANEL_TYPE"]] == "y", ][["COL"]]
+    if (y.pos == "left") {
       .widths <- rev(.widths)
     }
-    unit(rep(.widths, ncol/2), "null")
+    unit(rep(.widths, ncol / 2), "null")
   } else {
     unit(rep(1, ncol), "null")
   }
-  p.heights <- if("x"%in% side_panels_present) {
-    .heights <- c(aspect_ratio, aspect_ratio*side.panel.scale.x)
-    .tmp <- layout[layout[["PANEL_TYPE"]]=="x",][["ROW"]]
-    if(x.pos=="top"){
+  p.heights <- if ("x" %in% side_panels_present) {
+    .heights <- c(aspect_ratio, aspect_ratio * side.panel.scale.x)
+    .tmp <- layout[layout[["PANEL_TYPE"]] == "x", ][["ROW"]]
+    if (x.pos == "top") {
       .heights <- rev(.heights)
     }
-    unit(rep(abs(.heights), nrow/2), "null")
+    unit(rep(abs(.heights), nrow / 2), "null")
   } else {
     unit(rep(aspect_ratio, nrow), "null")
   }
   panel_table <- gtable_matrix("layout", panel_table,
-                               widths = p.widths,
-                               heights = p.heights, respect = respect,
-                               clip = coord$clip, z = matrix(1, ncol = ncol, nrow = nrow))
-  panel_table$layout$name <- paste0('panel-', rep(seq_len(ncol), nrow), '-', rep(seq_len(nrow), each = ncol))
+    widths = p.widths,
+    heights = p.heights, respect = respect,
+    clip = coord$clip, z = matrix(1, ncol = ncol, nrow = nrow)
+  )
+  panel_table$layout$name <- paste0("panel-", rep(seq_len(ncol), nrow), "-", rep(seq_len(nrow), each = ncol))
 
-  #need to register theme element
+  # need to register theme element
   sidepanel.spacing.x <- calc_element("ggside.panel.spacing.x", theme)
   xpanel_spacing <- calc_element("panel.spacing.x", theme)
   sidepanel.spacing.y <- calc_element("ggside.panel.spacing.y", theme)
   ypanel_spacing <- calc_element("panel.spacing.y", theme)
-  col.widths <- if("y"%in%side_panels_present){
-    unit(rep(c(sidepanel.spacing.x, xpanel_spacing), length.out = length(panel_table$widths)-1), "pt")
+  col.widths <- if ("y" %in% side_panels_present) {
+    unit(rep(c(sidepanel.spacing.x, xpanel_spacing), length.out = length(panel_table$widths) - 1), "pt")
   } else {
     xpanel_spacing
   }
   panel_table <- gtable_add_col_space(panel_table, col.widths)
-  row.heights <- if("x"%in%side_panels_present){
-    unit(rep(c(sidepanel.spacing.y, ypanel_spacing), length.out = length(panel_table$heights)-1), "pt")
+  row.heights <- if ("x" %in% side_panels_present) {
+    unit(rep(c(sidepanel.spacing.y, ypanel_spacing), length.out = length(panel_table$heights) - 1), "pt")
   } else {
     ypanel_spacing
   }
@@ -107,64 +108,74 @@ sideFacetNull_draw_panels <- function(panels, layout, x_scales, y_scales,
 
   .xgroupby <- "COL"
   .ygroupby <- "ROW"
-  bottom <- do_by(layout, "COL", function(x, on){
+  bottom <- do_by(layout, "COL", function(x, on) {
     x[["ROW2"]] <- switch(on,
-                          default = max(x[["ROW"]]),
-                          main = max(x[["ROW"]][x[["PANEL_TYPE"]]!="x"]),
-                          side = max(x[["ROW"]][x[["PANEL_TYPE"]]!="main"]))
-    x}, on = params$ggside$draw_x_on)
-  top <- do_by(layout, "COL", function(x, on){
+      default = max(x[["ROW"]]),
+      main = max(x[["ROW"]][x[["PANEL_TYPE"]] != "x"]),
+      side = max(x[["ROW"]][x[["PANEL_TYPE"]] != "main"])
+    )
+    x
+  }, on = params$ggside$draw_x_on)
+  top <- do_by(layout, "COL", function(x, on) {
     x[["ROW2"]] <- switch(on,
-                          default = min(x[["ROW"]]),
-                          main = min(x[["ROW"]][x[["PANEL_TYPE"]]!="x"]),
-                          side = min(x[["ROW"]][x[["PANEL_TYPE"]]!="main"]))
-    x}, on = params$ggside$draw_x_on)
-  right <- do_by(layout, "ROW", function(x, on){
+      default = min(x[["ROW"]]),
+      main = min(x[["ROW"]][x[["PANEL_TYPE"]] != "x"]),
+      side = min(x[["ROW"]][x[["PANEL_TYPE"]] != "main"])
+    )
+    x
+  }, on = params$ggside$draw_x_on)
+  right <- do_by(layout, "ROW", function(x, on) {
     x[["COL2"]] <- switch(on,
-                          default = max(x[["COL"]]),
-                          main = max(x[["COL"]][!x[["PANEL_TYPE"]]=="y"]),
-                          side = max(x[["COL"]][!x[["PANEL_TYPE"]]=="main"]))
-    x}, on = params$ggside$draw_y_on)
-  left <- do_by(layout, "ROW", function(x, on){
+      default = max(x[["COL"]]),
+      main = max(x[["COL"]][!x[["PANEL_TYPE"]] == "y"]),
+      side = max(x[["COL"]][!x[["PANEL_TYPE"]] == "main"])
+    )
+    x
+  }, on = params$ggside$draw_y_on)
+  left <- do_by(layout, "ROW", function(x, on) {
     x[["COL2"]] <- switch(on,
-                          default = min(x[["COL"]]),
-                          main = min(x[["COL"]][!x[["PANEL_TYPE"]]=="y"]),
-                          side = min(x[["COL"]][!x[["PANEL_TYPE"]]=="main"]))
-    x}, on = params$ggside$draw_y_on)
+      default = min(x[["COL"]]),
+      main = min(x[["COL"]][!x[["PANEL_TYPE"]] == "y"]),
+      side = min(x[["COL"]][!x[["PANEL_TYPE"]] == "main"])
+    )
+    x
+  }, on = params$ggside$draw_y_on)
 
-  if(params$ggside$scales%in%c("free","free_y")){ #if y is free, include x PANELS_TYPES
-    right <- right[right[["COL"]]==right[["COL2"]]|right[["PANEL_TYPE"]]=="x",]
-    left <- left[left[["COL"]]==left[["COL2"]]|left[["PANEL_TYPE"]]=="x",]
+  if (params$ggside$scales %in% c("free", "free_y")) { # if y is free, include x PANELS_TYPES
+    right <- right[right[["COL"]] == right[["COL2"]] | right[["PANEL_TYPE"]] == "x", ]
+    left <- left[left[["COL"]] == left[["COL2"]] | left[["PANEL_TYPE"]] == "x", ]
   } else {
-    right <- right[right[["COL"]]==right[["COL2"]],]
-    left <- left[left[["COL"]]==left[["COL2"]],]
+    right <- right[right[["COL"]] == right[["COL2"]], ]
+    left <- left[left[["COL"]] == left[["COL2"]], ]
   }
-  if(params$ggside$scales%in%c("free","free_x")){ #if x is free, include y PANELS_TYPES
-    top <- top[top[["ROW"]]==top[["ROW2"]]|top[["PANEL_TYPE"]]=="y",]
-    bottom <- bottom[bottom[["ROW"]]==bottom[["ROW2"]]|bottom[["PANEL_TYPE"]]=="y",]
+  if (params$ggside$scales %in% c("free", "free_x")) { # if x is free, include y PANELS_TYPES
+    top <- top[top[["ROW"]] == top[["ROW2"]] | top[["PANEL_TYPE"]] == "y", ]
+    bottom <- bottom[bottom[["ROW"]] == bottom[["ROW2"]] | bottom[["PANEL_TYPE"]] == "y", ]
   } else {
-    top <- top[top[["ROW"]]==top[["ROW2"]],]
-    bottom <- bottom[bottom[["ROW"]]==bottom[["ROW2"]],]
+    top <- top[top[["ROW"]] == top[["ROW2"]], ]
+    bottom <- bottom[bottom[["ROW"]] == bottom[["ROW2"]], ]
   }
 
-  #top, left, bottom, right, variables includes panels that would have axis shown
-  #for their relavent positions.
-  #Do an anti_join against layout to find panels that should get a zeroGrobe
-  bottom <- anti_join(layout, bottom, by = c("ROW","COL"))[["panel_pos"]]
-  top <- anti_join(layout, top, by = c("ROW","COL"))[["panel_pos"]]
-  right <- anti_join(layout, right, by = c("ROW","COL"))[["panel_pos"]]
-  left <- anti_join(layout, left, by = c("ROW","COL"))[["panel_pos"]]
-  #pulled panel positions
-  #Place ZeroGrobs
-  axis_mat_x_top[top]<- list(zeroGrob())
-  axis_mat_x_bottom[bottom]<- list(zeroGrob())
+  # top, left, bottom, right, variables includes panels that would have axis shown
+  # for their relavent positions.
+  # Do an anti_join against layout to find panels that should get a zeroGrobe
+  bottom <- anti_join(layout, bottom, by = c("ROW", "COL"))[["panel_pos"]]
+  top <- anti_join(layout, top, by = c("ROW", "COL"))[["panel_pos"]]
+  right <- anti_join(layout, right, by = c("ROW", "COL"))[["panel_pos"]]
+  left <- anti_join(layout, left, by = c("ROW", "COL"))[["panel_pos"]]
+  # pulled panel positions
+  # Place ZeroGrobs
+  axis_mat_x_top[top] <- list(zeroGrob())
+  axis_mat_x_bottom[bottom] <- list(zeroGrob())
   axis_mat_y_left[left] <- list(zeroGrob())
   axis_mat_y_right[right] <- list(zeroGrob())
 
-  #calculate spacing based on ggside params and layout
-  spacing <- calc_panel_spacing(ggside = params$ggside, layout = layout,
-                                top = axis_mat_x_top, right = axis_mat_y_right,
-                                bot = axis_mat_x_bottom, left = axis_mat_y_left)
+  # calculate spacing based on ggside params and layout
+  spacing <- calc_panel_spacing(
+    ggside = params$ggside, layout = layout,
+    top = axis_mat_x_top, right = axis_mat_y_right,
+    bot = axis_mat_x_bottom, left = axis_mat_y_left
+  )
   panel_table <- weave_tables_row(panel_table, axis_mat_x_top, -1, spacing$top, "axis-t", 3)
   panel_table <- weave_tables_row(panel_table, axis_mat_x_bottom, 0, spacing$bot, "axis-b", 3)
   panel_table <- weave_tables_col(panel_table, axis_mat_y_left, -1, spacing$left, "axis-l", 3)
@@ -173,16 +184,18 @@ sideFacetNull_draw_panels <- function(panels, layout, x_scales, y_scales,
   panel_table
 }
 
-sideFacetNull_map_data <- function (data, layout, params) {
-  if (is.waive(data))
+sideFacetNull_map_data <- function(data, layout, params) {
+  if (is.waive(data)) {
     return(data_frame0(PANEL = factor()))
-  if (empty(data))
+  }
+  if (empty(data)) {
     return(data_frame0(!!!c(data, list(PANEL = factor()))))
+  }
 
-  if(!"PANEL_TYPE"%in%colnames(data)){
+  if (!"PANEL_TYPE" %in% colnames(data)) {
     data$PANEL_TYPE <- "main"
   }
-  layout <- unwrap(layout, c("ROW","COL"), "FACET_VARS")
+  layout <- unwrap(layout, c("ROW", "COL"), "FACET_VARS")
   keys <- join_keys(data, layout, by = "PANEL_TYPE")
   data[["PANEL"]] <- layout[["PANEL"]][match(keys$x, keys$y)]
   data
